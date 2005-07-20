cafb6251055c6dc44616c33ea4c7bd56b0526b1f
regnault <regnault>
2005-07-20 21:13:06 +0000

    Nicolas Regnault: - start adding grid search for BosonsV2V0Overlap
    git-svn-id: https://www.nick-ux.org/diagham/svn/trunk@285 b9410c57-6ef9-0310-98bc-a73ba792996a

--- scripts/QHE/BosonsV2V0Overlap.pl
+++ scripts/QHE/BosonsV2V0Overlap.pl
@@ -70,11 +70,6 @@ unless (open (OUTFILE, ">".$DataFileName.".overlap"))
   }
 close(OUTFILE);
 
-my $ParityValue = 0;
-if ((($NbrBosons % 2) == 1) && (($LzMax % 2) == 1))
-  {
-    $ParityValue = 1;
-  }
 
 if ($GridFile eq "")
   {
@@ -84,8 +79,8 @@ if ($GridFile eq "")
 	  {
 	    die ("can't open tmppseudopotential.dat\n");
 	  }
-	print OUTFILE "Pseudopotentials = 1 0 0.158 0 0 0 ".$MinValue;
-	my $TmpLz = 7;
+	print OUTFILE "Pseudopotentials = 1 0 ".$MinValue;
+	my $TmpLz = 3;
 	while ($TmpLz <= $LzMax)
 	  {
 	    print OUTFILE " 0";
@@ -96,48 +91,28 @@ if ($GridFile eq "")
 	
 	my $DiagOutputFileName = $MinValue;
 	$DiagOutputFileName =~ s/\./\_/;
-	my $Command = $PathToDiagonalizationProgram." -p ".$NbrBosons." -l ".$LzMax." --nbr-lz 1 -n 1 --force-reorthogonalize --eigenstate --interaction-name v2v0".$DiagOutputFileName." --interaction-file tmppseudopotential.dat ".$DiagonalizationProgramOptions;
-	`$Command`;
-	$DiagOutputFileName = "bosons_v2v0".$DiagOutputFileName."_n_".$NbrBosons."_2s_".$LzMax."_lz";
-	if (-e $DiagOutputFileName."_".$ParityValue.".0.vec")
+	$DiagOutputFileName = "v2v0".$DiagOutputFileName;
+	my $Overlap = &EvalauteOverlap($PathToDiagonalizationProgram, $PathToOverlapProgram, $NbrBosons, $LzMax, $ReferenceVector, $DiagOutputFileName, $KeepFlag);
+	unless (open (OUTFILE, ">>".$DataFileName.".overlap"))
 	  {
-	    $Command = $PathToOverlapProgram." -p ".$NbrBosons." -l ".$LzMax." --exact-state ".$ReferenceVector." --use-exact ".$DiagOutputFileName."_".$ParityValue.".0.vec";
-	    my $Overlap = `$Command`;
-	    chomp ($Overlap);
-	    $Overlap =~ s/^overlap \= //; 
-	    unless (open (OUTFILE, ">>".$DataFileName.".overlap"))
-	      {
-		die ("can't open ".$DataFileName.".overlap\n");
-	      }
-	    print OUTFILE $MinValue." ".($Overlap * $Overlap)."\n";
-	    close(OUTFILE);
+	    die ("can't open ".$DataFileName.".overlap\n");
 	  }
-	if ($KeepFlag == 0)
-	  {
-	    if (-e $DiagOutputFileName."_".$ParityValue.".0.vec")
-	      {
-		unlink($DiagOutputFileName."_".$ParityValue.".0.vec")
-	      }
-	    if (-e $DiagOutputFileName.".dat")
-	      {
-		unlink ($DiagOutputFileName.".dat");
-	      }
-	  }
+	print OUTFILE $MinValue." ".$Overlap."\n";
+	close(OUTFILE);
+
+	unlink("tmppseudopotential.dat");
+	$NbrValues--;
+	$MinValue += $Step;
       }
-    $NbrValues--;
-    $MinValue += $Step;
-  }
-
-
-
-if ($PlotFlag == 1)
-  {
-    $MinValue += $Step;
-    unless (open (OUTFILE, ">".$DataFileName.".p"))
+    
+    if ($PlotFlag == 1)
       {
-	die ("can't create file ".$DataFileName.".p\n");
-      }
-    print OUTFILE ("set xrange [0:".$MinValue."]
+	$MinValue += $Step;
+	unless (open (OUTFILE, ">".$DataFileName.".p"))
+	  {
+	    die ("can't create file ".$DataFileName.".p\n");
+	  }
+	print OUTFILE ("set xrange [0:".$MinValue."]
 set yrange [0:1.1]
 set xlabel \"V2/V0\"
 set ylabel \"overlap\"
@@ -146,8 +121,141 @@ set terminal postscript landscape enhanced \"Helvetica\" 14
 set output \"".$DataFileName.".ps\"
 plot \"".$DataFileName.".overlap\" using 1:2 title \"N=".$NbrBosons." 2S=".$LzMax."\", \"".$DataFileName.".overlap\" using 1:2 notitle with lines
 ");  
-    my $Command = "gnuplot ".$DataFileName.".p";
-    `$Command`;
-    unlink ($DataFileName.".p");
+	my $Command = "gnuplot ".$DataFileName.".p";
+	`$Command`;
+	unlink ($DataFileName.".p");
+      }
+  }
+else
+  {
+    my @GridVertices;
+    &ParseGridDefinition ($GridFile, \@GridVertices);
+    if ($#GridVertices == 0)
+      {
+	die ($GridFile." is an invalid grid file\n");
+      }
+    my $VertexCount = 0;
+    my $VertexPosition;
+    foreach $VertexPosition (@GridVertices)
+      {
+	unless (open (OUTFILE, ">tmppseudopotential.dat"))
+	  {
+	    die ("can't open tmppseudopotential.dat\n");
+	  }
+	print OUTFILE "Pseudopotentials = ".$VertexPosition."\n";
+	close(OUTFILE);
+
+	my $DiagOutputFileName = "v2v0".$VertexCount;
+	my $Overlap = 0.0;
+#&EvalauteOverlap($PathToDiagonalizationProgram, $PathToOverlapProgram, $NbrBosons, $LzMax, $ReferenceVector, $DiagOutputFileName, $KeepFlag);
+
+	unless (open (OUTFILE, ">>".$DataFileName.".overlap"))
+	  {
+	    die ("can't open ".$DataFileName.".overlap\n");
+	  }
+	print OUTFILE $VertexPosition."|".$Overlap."\n";
+	close(OUTFILE);
+
+	unlink("tmppseudopotential.dat");
+	$VertexCount++;
+      }
   }
 
+
+# evaluate overlap betwwen maximally symmetric ground state evalauted for a given pseudopotential configuration, and a given eigenstate
+#
+# $_[0] = diagonalization program (with full path if any)
+# $_[1] = overlap program (with full path if any)
+# $_[2] = number of bosons
+# $_[3] = maximum Lz value
+# $_[4] = file that contains the reference vector (with path)
+# $_[5] = interaction name
+# $_[6] = flag to indicate if partial datas have to be kept (0 if false)
+# return value = squqre overlap
+
+sub EvalauteOverlap()
+  {
+    my $PathToDiagonalizationProgram = $_[0];
+    my $PathToOverlapProgram = $_[1];
+    my $NbrBosons = $_[2];
+    my $LzMax = $_[3];
+    my $ReferenceVector = $_[4];
+    my $DiagOutputFileName = $_[5];
+    my $KeepFlag = $_[6];
+
+
+    my $ParityValue = 0;
+    if ((($NbrBosons % 2) == 1) && (($LzMax % 2) == 1))
+      {
+	$ParityValue = 1;
+      }
+
+    my $Command = $PathToDiagonalizationProgram." -p ".$NbrBosons." -l ".$LzMax." --nbr-lz 1 -n 1 --force-reorthogonalize --eigenstate --interaction-name ".$DiagOutputFileName." --interaction-file tmppseudopotential.dat ".$DiagonalizationProgramOptions;
+    `$Command`;
+
+    $DiagOutputFileName = "bosons_".$DiagOutputFileName."_n_".$NbrBosons."_2s_".$LzMax."_lz";
+
+    my $Overlap = -1.0;
+    if (-e $DiagOutputFileName."_".$ParityValue.".0.vec")
+      {
+	$Command = $PathToOverlapProgram." -p ".$NbrBosons." -l ".$LzMax." --exact-state ".$ReferenceVector." --use-exact ".$DiagOutputFileName."_".$ParityValue.".0.vec";
+	$Overlap = `$Command`;
+	chomp ($Overlap);
+	$Overlap =~ s/^overlap \= //; 
+	$Overlap *= $Overlap;
+      }
+
+    if ($KeepFlag == 0)
+      {
+	if (-e $DiagOutputFileName."_".$ParityValue.".0.vec")
+	  {
+	    unlink($DiagOutputFileName."_".$ParityValue.".0.vec")
+	  }
+	if (-e $DiagOutputFileName.".dat")
+	  {
+	    unlink ($DiagOutputFileName.".dat");
+	  }
+      }
+    return $Overlap;
+  }
+
+
+# parse a grid definition file
+#
+# $_[0] = name file that contains the grid definition
+# $_[1] = reference on the array where vertex positions will be stored (each position is a string that gives pseudo-potential definition)
+# $_[2] = maximum Lz value
+
+sub ParseGridDefinition()
+  {
+    my $GridFile = $_[0];
+    my $GridVertices = $_[1];
+    my $LzMax = $_[2];
+
+    unless (open (INFILE, $GridFile))
+      {
+	die ("can't open ".$GridFile."\n");
+      }
+    my @Pseudopotentials;
+    my $TmpLine;
+    while (defined($TmpLine = <INFILE>))
+      {
+	chomp ($TmpLine);
+	$TmpLine =~ s/^\s+//;
+	$TmpLine =~ s/\s+$//;
+	$TmpLine =~ s/^\#.*//;
+	if ($TmpLine ne "")
+	  {
+	    my @TmpArray = split (/\s+/, $TmpLine);
+	    if (($#TmpArray != 4) || (!($TmpArray[0] =~ /^\d+$/)) || (!($TmpArray[1] =~ /^\-?\d*\.?\d+$/)) || (!($TmpArray[2] =~ /^\-?\d*\.?\d+$/))
+		|| (!($TmpArray[3] =~ /^\d+$/)))
+	      {
+		close (INFILE);
+		return;
+	      }
+	    my $Index = shift (@TmpArray);
+	    $Pseudopotentials[$Index] = \@TmpArray;
+	  }
+      }
+    close (INFILE);
+  }
