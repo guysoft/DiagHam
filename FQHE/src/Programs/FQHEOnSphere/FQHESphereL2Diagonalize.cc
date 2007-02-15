028f3ea6e1969926fb1db0e86a44af215302d355
regnault <regnault>
2007-02-15 17:11:45 +0000

    Nicolas Regnault: - add pure L^2 Hamiltonian and the corresponding program.
    git-svn-id: https://www.nick-ux.org/diagham/svn/trunk@516 b9410c57-6ef9-0310-98bc-a73ba792996a

---
+++ FQHE/src/Programs/FQHEOnSphere/FQHESphereL2Diagonalize.cc
@@ -0,0 +1,235 @@
+#include "HilbertSpace/FermionOnSphere.h"
+#include "HilbertSpace/FermionOnSphereUnlimited.h"
+#include "HilbertSpace/FermionOnSphereHaldaneBasis.h"
+#include "HilbertSpace/BosonOnSphere.h"
+
+#include "Hamiltonian/ParticleOnSphereL2Hamiltonian.h"
+
+#include "Architecture/ArchitectureManager.h"
+#include "Architecture/AbstractArchitecture.h"
+#include "Architecture/ArchitectureOperation/MainTaskOperation.h"
+
+#include "MainTask/QHEOnSphereMainTask.h"
+
+#include "Options/OptionManager.h"
+#include "Options/OptionGroup.h"
+#include "Options/AbstractOption.h"
+#include "Options/BooleanOption.h"
+#include "Options/SingleIntegerOption.h"
+#include "Options/SingleDoubleOption.h"
+#include "Options/SingleStringOption.h"
+
+#include "GeneralTools/ConfigurationParser.h"
+
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+#include <stdio.h>
+
+
+using std::ios;
+using std::cout;
+using std::endl;
+using std::ofstream;
+
+
+int main(int argc, char** argv)
+{
+  cout.precision(14);
+
+  // some running options and help
+  OptionManager Manager ("FQHESphereL2Diagonalize" , "0.01");
+  OptionGroup* LanczosGroup  = new OptionGroup ("Lanczos options");
+  OptionGroup* ToolsGroup  = new OptionGroup ("tools options");
+  OptionGroup* MiscGroup = new OptionGroup ("misc options");
+  OptionGroup* SystemGroup = new OptionGroup ("system options");
+  OptionGroup* PrecalculationGroup = new OptionGroup ("precalculation options");
+
+  ArchitectureManager Architecture;
+
+  Manager += SystemGroup;
+  Architecture.AddOptionGroup(&Manager);
+  Manager += LanczosGroup;
+  Manager += ToolsGroup;
+  Manager += PrecalculationGroup;
+  Manager += MiscGroup;
+
+  (*SystemGroup) += new SingleIntegerOption  ('p', "nbr-particles", "number of particles", 5);
+  (*SystemGroup) += new SingleIntegerOption  ('l', "lzmax", "twice the maximum momentum for a single particle", 8);
+  (*SystemGroup) += new SingleIntegerOption  ('z', "total-lz", "twice the total momentum projection for the system", 0);
+  (*SystemGroup) += new  SingleStringOption ('\n', "interaction-name", "interaction name (as it should appear in output files)", "l2");
+  (*SystemGroup) += new BooleanOption  ('\n', "haldane", "use Haldane basis instead of the usual n-body basis");
+  (*SystemGroup) += new SingleStringOption  ('\n', "reference-state", "reference state to start the Haldane algorithm from (can be laughlin, pfaffian or readrezayi3)", "laughlin");
+  (*SystemGroup) += new SingleStringOption  ('\n', "reference-file", "use a file as the definition of the reference state");
+  (*SystemGroup) += new BooleanOption  ('\n', "boson", "use bosonic statistics instead of fermionic statistic");
+  (*SystemGroup) += new SingleDoubleOption ('\n', "l2-factor", "multiplicative factor in front of an optional L^2 operator than can be added to the Hamiltonian", 1.0);
+  (*SystemGroup) += new SingleDoubleOption ('\n', "energy-shift", "if non zero, override energy shift using the indicated value ", -10.0);
+
+  (*LanczosGroup) += new SingleIntegerOption  ('n', "nbr-eigen", "number of eigenvalues", 30);
+  (*LanczosGroup)  += new SingleIntegerOption  ('\n', "full-diag", 
+						"maximum Hilbert space dimension for which full diagonalization is applied", 
+						500, true, 100);
+  (*LanczosGroup) += new SingleIntegerOption  ('\n', "iter-max", "maximum number of lanczos iteration", 3000);
+  (*LanczosGroup)  += new BooleanOption  ('\n', "block-lanczos", "use block Lanczos algorithm", false);
+  (*LanczosGroup)  += new SingleIntegerOption  ('\n', "block-size", "size of the block used in the block Lanczos algorithm", 2);  
+  (*LanczosGroup)  += new BooleanOption  ('d', "disk", "enable disk resume capabilities", false);
+  (*LanczosGroup) += new BooleanOption  ('r', "resume", "resume from disk datas", false);
+  (*LanczosGroup) += new SingleIntegerOption  ('i', "nbr-iter", "number of lanczos iteration (for the current run)", 10);
+  (*LanczosGroup) += new SingleIntegerOption  ('\n', "nbr-vector", "maximum number of vector in RAM during Lanczos iteration", 10);
+  (*LanczosGroup) += new BooleanOption  ('\n', "force-reorthogonalize", 
+					 "force to use Lanczos algorithm with reorthogonalizion even if the number of eigenvalues to evaluate is 1", false);
+  (*LanczosGroup) += new BooleanOption  ('\n', "eigenstate", "evaluate eigenstates", false);  
+  (*LanczosGroup) += new BooleanOption  ('\n', "eigenstate-convergence", "evaluate Lanczos convergence from eigenstate convergence", false);  
+  (*LanczosGroup) += new BooleanOption  ('\n', "show-itertime", "show time spent for each Lanczos iteration", false); 
+  (*LanczosGroup) += new SingleStringOption  ('\n', "initial-vector", "use file as the initial vector for the Lanczos algorithm" , 0);
+  (*LanczosGroup) += new  BooleanOption ('\n', "partial-lanczos", "only run a given number of Lanczos iterations" , false);
+  (*PrecalculationGroup) += new BooleanOption ('\n', "disk-cache", "use disk cache for fast multiplication", false);
+  (*PrecalculationGroup) += new SingleIntegerOption  ('m', "memory", "amount of memory that can be allocated for fast multiplication (in Mbytes)", 500);
+  (*PrecalculationGroup) += new SingleStringOption  ('\n', "load-precalculation", "load precalculation from a file",0);
+  (*PrecalculationGroup) += new SingleStringOption  ('\n', "save-precalculation", "save precalculation in a file",0);
+  (*PrecalculationGroup) += new SingleIntegerOption  ('\n', "fast-search", "amount of memory that can be allocated for fast state search (in Mbytes)", 9);
+#ifdef __LAPACK__
+  (*ToolsGroup) += new BooleanOption  ('\n', "use-lapack", "use LAPACK libraries instead of DiagHam libraries");
+#endif
+  (*ToolsGroup) += new BooleanOption  ('\n', "show-hamiltonian", "show matrix representation of the hamiltonian");
+  (*MiscGroup) += new BooleanOption  ('h', "help", "display this help");
+  
+  if (Manager.ProceedOptions(argv, argc, cout) == false)
+    {
+      cout << "see man page for option syntax or type FQHESphereL2Diagonalize -h" << endl;
+      return -1;
+    }
+  if (((BooleanOption*) Manager["help"])->GetBoolean() == true)
+    {
+      Manager.DisplayHelp (cout);
+      return 0;
+    }
+
+
+  int NbrParticles = ((SingleIntegerOption*) Manager["nbr-particles"])->GetInteger();
+  int LzMax = ((SingleIntegerOption*) Manager["lzmax"])->GetInteger();
+  int TotalLz  = ((SingleIntegerOption*) Manager["total-lz"])->GetInteger();
+  long Memory = ((unsigned long) ((SingleIntegerOption*) Manager["memory"])->GetInteger()) << 20;
+  unsigned long MemorySpace = ((unsigned long) ((SingleIntegerOption*) Manager["fast-search"])->GetInteger()) << 20;
+  char* LoadPrecalculationFileName = ((SingleStringOption*) Manager["load-precalculation"])->GetString();  
+  bool DiskCacheFlag = ((BooleanOption*) Manager["disk-cache"])->GetBoolean();
+  bool FirstRun = true;
+  bool HaldaneBasisFlag = ((BooleanOption*) Manager["haldane"])->GetBoolean();
+
+  char* OutputNameLz = new char [256 + strlen(((SingleStringOption*) Manager["interaction-name"])->GetString())];
+  sprintf (OutputNameLz, "fermions_%s_n_%d_2s_%d_lz.dat", ((SingleStringOption*) Manager["interaction-name"])->GetString(), NbrParticles, LzMax);
+
+  ParticleOnSphere* Space;
+  if (((BooleanOption*) Manager["boson"])->GetBoolean() == false)
+    {
+      if (HaldaneBasisFlag == false)
+	{
+#ifdef __64_BITS__
+	  if (LzMax <= 63)
+	    Space = new FermionOnSphere(NbrParticles, TotalLz, LzMax, MemorySpace);
+	  else
+	    Space = new FermionOnSphereUnlimited(NbrParticles, TotalLz, LzMax, MemorySpace);
+#else
+	  if (LzMax <= 31)
+	    Space = new FermionOnSphere(NbrParticles, TotalLz, LzMax, MemorySpace);
+	  else
+	    Space = new FermionOnSphereUnlimited(NbrParticles, TotalLz, LzMax, MemorySpace);
+#endif
+	}
+      else
+	{
+	  int* ReferenceState = 0;
+	  if (((SingleStringOption*) Manager["reference-file"])->GetString() == 0)
+	    {
+	      ReferenceState = new int[LzMax + 1];
+	      for (int i = 0; i <= LzMax; ++i)
+		ReferenceState[i] = 0;
+	      if (strcasecmp(((SingleStringOption*) Manager["reference-state"])->GetString(), "laughlin") == 0)
+		for (int i = 0; i <= LzMax; i += 3)
+		  ReferenceState[i] = 1;
+	      else
+		if (strcasecmp(((SingleStringOption*) Manager["reference-state"])->GetString(), "pfaffian") == 0)
+		  for (int i = 0; i <= LzMax; i += 4)
+		    {
+		      ReferenceState[i] = 1;
+		      ReferenceState[i + 1] = 1;
+		    }
+		else
+		  if (strcasecmp(((SingleStringOption*) Manager["reference-state"])->GetString(), "readrezayi3") == 0)
+		    for (int i = 0; i <= LzMax; i += 5)
+		      {
+			ReferenceState[i] = 1;
+			ReferenceState[i + 1] = 1;
+			ReferenceState[i + 2] = 1;
+		      }
+		  else
+		    {
+		      cout << "unknown reference state " << ((SingleStringOption*) Manager["reference-state"])->GetString() << endl;
+		      return -1;
+		    }
+	    }
+	  else
+	    {
+	      ConfigurationParser ReferenceStateDefinition;
+	      if (ReferenceStateDefinition.Parse(((SingleStringOption*) Manager["reference-file"])->GetString()) == false)
+		{
+		  ReferenceStateDefinition.DumpErrors(cout) << endl;
+		  return -1;
+		}
+	      if ((ReferenceStateDefinition.GetAsSingleInteger("NbrParticles", NbrParticles) == false) || (NbrParticles <= 0))
+		{
+		  cout << "NbrParticles is not defined or as a wrong value" << endl;
+		  return -1;
+		}
+	      if ((ReferenceStateDefinition.GetAsSingleInteger("LzMax", LzMax) == false) || (LzMax <= 0))
+		{
+		  cout << "LzMax is not defined or as a wrong value" << endl;
+		  return -1;
+		}
+	      int MaxNbrLz;
+	      if (ReferenceStateDefinition.GetAsIntegerArray("ReferenceState", ' ', ReferenceState, MaxNbrLz) == false)
+		{
+		  cout << "error while parsing ReferenceState in " << ((SingleStringOption*) Manager["reference-file"])->GetString() << endl;
+		  return -1;     
+		}
+	      if (MaxNbrLz != (LzMax + 1))
+		{
+		  cout << "wrong LzMax value in ReferenceState" << endl;
+		  return -1;     
+		}
+	    }
+	  Space = new FermionOnSphereHaldaneBasis(NbrParticles, TotalLz, LzMax, ReferenceState);
+	}
+    }
+  else
+    {
+      Space = new BosonOnSphere(NbrParticles, TotalLz, LzMax);
+    }
+
+  Architecture.GetArchitecture()->SetDimension(Space->GetHilbertSpaceDimension());
+  AbstractQHEOnSphereHamiltonian* Hamiltonian = new ParticleOnSphereL2Hamiltonian(Space, NbrParticles, LzMax, TotalLz,
+										  Architecture.GetArchitecture(), 
+										  ((SingleDoubleOption*) Manager["l2-factor"])->GetDouble(),
+										  Memory, DiskCacheFlag,
+										  LoadPrecalculationFileName);
+
+  double Shift = ((SingleDoubleOption*) Manager["energy-shift"])->GetDouble();
+  Hamiltonian->ShiftHamiltonian(Shift);
+  char* EigenvectorName = 0;
+  if (((BooleanOption*) Manager["eigenstate"])->GetBoolean() == true)	
+    {
+      EigenvectorName = new char [64];
+      sprintf (EigenvectorName, "fermions_%s_n_%d_2s_%d_lz_%d", ((SingleStringOption*) Manager["interaction-name"])->GetString(), NbrParticles, LzMax, TotalLz);
+    }
+  QHEOnSphereMainTask Task (&Manager, Space, Hamiltonian, TotalLz, Shift, OutputNameLz, FirstRun, EigenvectorName, LzMax);
+  MainTaskOperation TaskOperation (&Task);
+  TaskOperation.ApplyOperation(Architecture.GetArchitecture());
+  if (EigenvectorName != 0)
+    delete[] EigenvectorName;
+  delete Hamiltonian;
+  if (FirstRun == true)
+    FirstRun = false;
+
+  return 0;
+}
