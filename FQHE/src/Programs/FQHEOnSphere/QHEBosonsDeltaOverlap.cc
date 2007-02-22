7b64a22219ddce4d912aed9e13966ac893df1a79
regnault <regnault>
2007-02-22 22:21:13 +0000

    Nicolas Regnault: - add export to disk of intermediate results for QHEBosonsDeltaOverlap
    git-svn-id: https://www.nick-ux.org/diagham/svn/trunk@517 b9410c57-6ef9-0310-98bc-a73ba792996a

--- FQHE/src/Programs/FQHEOnSphere/QHEBosonsDeltaOverlap.cc
+++ FQHE/src/Programs/FQHEOnSphere/QHEBosonsDeltaOverlap.cc
@@ -74,6 +74,8 @@ int main(int argc, char** argv)
 
   (*MonteCarloGroup) += new SingleIntegerOption  ('i', "nbr-iter", "number of Monte Carlo iterations", 10000);
   (*MonteCarloGroup) += new SingleIntegerOption  ('\n', "display-step", "number of iteration between two consecutive result displays", 1000);
+  (*MonteCarloGroup) += new SingleIntegerOption  ('\n', "record-step", "number of iteration between two consecutive result recording of energy value (0 if no on-disk recording is needed)", 0);
+  (*MonteCarloGroup) += new SingleStringOption ('\n', "record-file", "name of the file where energy recording has to be done", "montecarlo.dat");
   (*MonteCarloGroup) += new BooleanOption  ('\n', "with-timecoherence", "use time coherence between two successive evaluation of the wave function");
   (*MonteCarloGroup) += new BooleanOption  ('\n', "show-details", "show intermediate values used for overlap calculation", false);
   (*MiscGroup) += new BooleanOption  ('h', "help", "display this help");
@@ -138,59 +140,20 @@ int main(int argc, char** argv)
   BosonOnSphere Space (NbrBosons, Lz, LzMax);
   ParticleOnSphereFunctionBasis Basis(LzMax);
   
-//  Abstract1DComplexFunction* TestWaveFunction = new LaughlinOnSphereWaveFunction(NbrBosons, 2);
-//  Abstract1DComplexFunction* TestWaveFunction = new PfaffianOnSphereWaveFunction(NbrBosons);
-//  Abstract1DComplexFunction* TestWaveFunction = new JainCFFilledLevelOnSphereWaveFunction(NbrBosons, 2, 1);
-//  Abstract1DComplexFunction* TestWaveFunction = new JainCFOnSphereWaveFunction("test.cf");
-//  Abstract1DComplexFunction* TestWaveFunction = new MooreReadOnSphereWaveFunction(NbrBosons, 3);
-//  Abstract1DComplexFunction* TestWaveFunction2 = new PfaffianOnSphereWaveFunction(NbrBosons);
   RealVector Location(2 * NbrBosons, true);
 
   AbstractRandomNumberGenerator* RandomNumber = new StdlibRandomNumberGenerator (29457);
 
   
-/*  ClebschGordanCoefficients Clebsch(3, 5);
-  cout << Clebsch.GetCoefficient(3, -5, 8) << endl;
-  cout << Clebsch.GetCoefficient(1, -3, 8) << endl;
-  cout << Clebsch.GetCoefficient(-1, -1, 8) << endl;
-  cout << Clebsch.GetCoefficient(-3, 1, 8) << endl;*/
-  ClebschGordanCoefficients Clebsch(4, 6);
-  cout << Clebsch.GetCoefficient(4, -4, 10) << endl;
-  cout << Clebsch.GetCoefficient(2, -2, 10) << endl;
-  cout << Clebsch.GetCoefficient(0, 0, 10) << endl;
-  cout << Clebsch.GetCoefficient(-2, 2, 10) << endl;
-  cout << Clebsch.GetCoefficient(-4, 4, 10) << endl;
-  
-/*  for (int k = 0; k < 10; ++k)
+  int RecordStep = ((SingleIntegerOption*) Manager["record-step"])->GetInteger();
+  Complex* RecordedOverlap = 0;
+  Complex* RecordedOverlapError = 0;
+  if (RecordStep != 0)
     {
-      for (int i = 0; i < NbrBosons; ++i)
-	{
-	  Location[i << 1] = M_PI * drand48();
-	  Location[(i << 1) + 1] = 2.0 * M_PI * drand48();
-	  cout << Location[i << 1] << " " << Location[(i << 1) + 1] << endl;
-	}
-      Location[0] = 0.5 * M_PI;
-      Location[1] =  0.0;
-      Location[2] = 0.5 * M_PI;
-      Location[3] =  0.5 * M_PI;
-      Location[4] = 0.5 * M_PI;
-      Location[5] =  M_PI; 
-      Location[6] = 0.5 * M_PI;
-      Location[7] = - 0.5 * M_PI;
-     
-      //  Location[4] = Location[0];
-      //  Location[5] = Location[1];
-      ParticleOnSphereFunctionBasis Basis(LzMax);
-      QHEParticleWaveFunctionOperation Operation(&Space, &State, &Location, &Basis);
-      Operation.ApplyOperation(Architecture.GetArchitecture());      
-      Complex ValueExact (Operation.GetScalar());
-      //      Complex ValueExact = Space.EvaluateWaveFunction(State, Location, Basis);
-      Complex ValueLaughlin = (*TestWaveFunction)(Location);
-//      cout << ValueExact  << endl; 
-      cout << ValueExact  << " " << ValueLaughlin << " " << (Norm(ValueExact) / Norm(ValueLaughlin)) << endl;        
-      cout << "-------------------------------------" << endl;
+      RecordedOverlap = new Complex [(NbrIter / RecordStep) + 1];
+      RecordedOverlapError = new Complex [(NbrIter / RecordStep) + 1];
     }
-  return 0;*/
+  int RecordIndex = 0;
   double Factor = 1.0;
   for (int j = 0; j < NbrBosons; ++j)
     {
@@ -261,6 +224,30 @@ int main(int argc, char** argv)
       ErrorNormalization += Tmp2 * Tmp2;
       NormalizationExact += Tmp2bis;
       ErrorNormalizationExact += Tmp2bis * Tmp2bis;
+      if ((i > 0) && ((RecordStep != 0) && ((i % RecordStep) == 0)))
+	{
+	  Complex Tmp4 = Overlap / ((double) i);
+	  Complex Tmp5 (sqrt( ((ErrorOverlap.Re / ((double) i)) - (Tmp4.Re * Tmp4.Re)) / ((double) i) ),
+			sqrt( ((ErrorOverlap.Im / ((double) i)) - (Tmp4.Im * Tmp4.Im)) / ((double) i) ));
+	  double Tmp6 = Normalization  / ((double) i);
+	  double Tmp7 = sqrt( ((ErrorNormalization / ((double) i))  -  (Tmp6 * Tmp6)) / ((double) i) );	  
+	  double Tmp8 = NormalizationExact  / ((double) i);
+	  double Tmp9 = sqrt( ((ErrorNormalizationExact / ((double) i))  -  (Tmp8 * Tmp8)) / ((double) i) );	  
+	  Tmp5.Re /= Tmp4.Re;
+	  Tmp5.Im /= Tmp4.Im;
+	  Tmp5.Re = fabs(Tmp5.Re);
+	  Tmp5.Im = fabs(Tmp5.Im);
+	  Tmp5.Re += (Tmp7 / Tmp6);
+	  Tmp5.Im += (Tmp7 / Tmp6);
+	  Tmp5.Re += (Tmp9 / Tmp8);
+	  Tmp5.Im += (Tmp9 / Tmp8);
+	  Tmp4 /= sqrt(Tmp6 * Tmp8);	  
+	  Tmp5.Re *= Tmp4.Re;
+	  Tmp5.Im *= Tmp4.Im;
+	  RecordedOverlap[RecordIndex] = Tmp4;
+	  RecordedOverlapError[RecordIndex] = Tmp5;
+	  ++RecordIndex;
+	}
       if ((i > 0) && ((i % (((SingleIntegerOption*) Manager["display-step"])->GetInteger())) == 0))
 	{
 	  cout << " i = " << i << endl;
@@ -324,7 +311,26 @@ int main(int argc, char** argv)
   Tmp5.Im *= Tmp4.Im;
   cout << Tmp4 << " +/- " << Tmp5 << endl;
   cout << "-----------------------------------------------" << endl;
- return 0;
+
+
+  if (RecordStep != 0)
+    {
+      RecordedOverlap[RecordIndex] = Tmp4;
+      RecordedOverlapError[RecordIndex] = Tmp5;
+      ofstream OverlapRecordFile;
+      OverlapRecordFile.precision(14);
+      OverlapRecordFile.open(((SingleStringOption*) Manager["record-file"])->GetString(), ios::out | ios::binary);
+      int NbrRecords = NbrIter / RecordStep;
+      OverlapRecordFile << "# Monte Carlo overlap calculation" << endl
+		       << "# step overlap.Re overlap.Im overlap2 error.Re error.Im error2" << endl;
+      for (int i = 0; i < NbrRecords; ++i)
+	OverlapRecordFile << i << " " << RecordedOverlap[i].Re << " " << RecordedOverlap[i].Im << " " << SqrNorm(RecordedOverlap[i])
+			  << " " << RecordedOverlapError[i].Re << " " <<  RecordedOverlapError[i].Im << " " 
+			  << (2 * ((RecordedOverlap[i].Re * RecordedOverlapError[i].Re) + (RecordedOverlap[i].Im * RecordedOverlapError[i].Im))) << endl;
+      OverlapRecordFile.close();
+    }
+
+  return 0;
 }
 
 
