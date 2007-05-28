0eeb64223dec31fdc851d7eb8a178cd3db979b78
(no author) <(no author)>
2007-05-28 16:08:20 +0000

    Nicolas Regnault: - update GetLValue to support Haldane and symmetric basis. Use improved L^2 calculation. Add code parallelization.
    git-svn-id: https://www.nick-ux.org/diagham/svn/trunk@584 b9410c57-6ef9-0310-98bc-a73ba792996a

--- FQHE/src/Programs/FQHEOnSphere/GetLValue.cc
+++ FQHE/src/Programs/FQHEOnSphere/GetLValue.cc
@@ -2,10 +2,6 @@
 
 #include "Vector/RealVector.h"
 
-#include "Matrix/RealTriDiagonalSymmetricMatrix.h"
-#include "Matrix/RealSymmetricMatrix.h"
-#include "Matrix/RealMatrix.h"
-
 #include "Options/OptionManager.h"
 #include "Options/OptionGroup.h"
 #include "Options/AbstractOption.h"
@@ -16,15 +12,26 @@
 
 #include "GeneralTools/ArrayTools.h"
 #include "GeneralTools/FilenameTools.h"
+#include "GeneralTools/ConfigurationParser.h"
+
+#include "Architecture/ArchitectureManager.h"
+#include "Architecture/AbstractArchitecture.h"
+#include "Architecture/ArchitectureOperation/MainTaskOperation.h"
 
 #include "Operator/ParticleOnSphereSquareTotalMomentumOperator.h"
 
-#include "Tools/FQHESpectrum/QHEOnSphereLzSortedSpectrum.h"
 #include "Tools/FQHEFiles/QHEOnSphereFileTools.h"
+
+#include "Architecture/ArchitectureOperation/VectorHamiltonianMultiplyOperation.h"
 
 #include "HilbertSpace/BosonOnSphere.h"
 #include "HilbertSpace/FermionOnSphere.h"
 #include "HilbertSpace/FermionOnSphereUnlimited.h"
+#include "HilbertSpace/FermionOnSphereSymmetricBasis.h"
+#include "HilbertSpace/FermionOnSphereHaldaneBasis.h"
+#include "HilbertSpace/FermionOnSphereHaldaneSymmetricBasis.h"
+
+#include "Hamiltonian/ParticleOnSphereL2Hamiltonian.h"
 
 #include <iostream>
 #include <stdlib.h>
@@ -42,27 +49,35 @@ int main(int argc, char** argv)
   cout.precision(14);
 
   // some running options and help
-  OptionManager Manager ("GetLValue" , "0.01");
+  OptionManager Manager ("FQHESphereLValue" , "0.01");
   OptionGroup* SystemGroup = new OptionGroup ("system options");
-  OptionGroup* DataGroup = new OptionGroup ("data options");
+  OptionGroup* PrecalculationGroup = new OptionGroup ("precalculation options");
   OptionGroup* MiscGroup = new OptionGroup ("misc options");
 
+  ArchitectureManager Architecture;
+
   Manager += SystemGroup;
-  Manager += DataGroup;
+  Architecture.AddOptionGroup(&Manager);
+  Manager += PrecalculationGroup;
   Manager += MiscGroup;
  
-  (*DataGroup) += new SingleStringOption  ('\0', "state", "name of the file that contains the state whose average L value has to be evaluated");
+  (*SystemGroup) += new SingleStringOption  ('\0', "state", "name of the file that contains the state whose average L value has to be evaluated");
   (*SystemGroup) += new SingleIntegerOption  ('p', "nbr-particles", "number of particles (0 if it has to be guessed from file name)", 0);
   (*SystemGroup) += new SingleIntegerOption  ('l', "lzmax", "twice the maximum momentum for a single particle (0 if it has to be guessed from file name)", 0);
-  (*SystemGroup) += new SingleIntegerOption  ('z', "lz", "twice the total lz value of the system (0 if it has to be guessed from file name)", 0);
+  (*SystemGroup) += new SingleIntegerOption  ('z', "total-lz", "twice the total lz value of the system (0 if it has to be guessed from file name)", 0);
   (*SystemGroup) += new SingleStringOption  ('s', "statistics", "particle statistics (bosons or fermions, try to guess it from file name if not defined)");
-  (*DataGroup) += new SingleDoubleOption  ('\n', "l-error", "error above which a vector is no more considerated as an eigenvector of L^2", 1e-12);
+  (*SystemGroup) += new BooleanOption  ('\n', "haldane", "use Haldane basis instead of the usual n-body basis");
+  (*SystemGroup) += new BooleanOption  ('\n', "symmetrized-basis", "use Lz <-> -Lz symmetrized version of the basis (only valid if total-lz=0)");
+  (*SystemGroup) += new SingleStringOption  ('\n', "reference-state", "reference state to start the Haldane algorithm from (can be laughlin, pfaffian or readrezayi3)", "laughlin");
+  (*SystemGroup) += new SingleStringOption  ('\n', "reference-file", "use a file as the definition of the reference state");
+  (*PrecalculationGroup) += new SingleStringOption  ('\n', "save-hilbert", "save Hilbert space description in the indicated file and exit (only available for the Haldane basis)",0);
+  (*PrecalculationGroup) += new SingleStringOption  ('\n', "load-hilbert", "load Hilbert space description from the indicated file (only available for the Haldane basis)",0);
 
   (*MiscGroup) += new BooleanOption  ('h', "help", "display this help");
 
   if (Manager.ProceedOptions(argv, argc, cout) == false)
     {
-      cout << "see man page for option syntax or type GetLValue -h" << endl;
+      cout << "see man page for option syntax or type FQHESphereLValue -h" << endl;
       return -1;
     }
   
@@ -74,17 +89,19 @@ int main(int argc, char** argv)
 
   if(((SingleStringOption*) Manager["state"])->GetString() == 0)
     {
-      cout << "no input state" << endl << "see man page for option syntax or type GetLValue -h" << endl;
+      cout << "no input state" << endl << "see man page for option syntax or type FQHESphereLValue -h" << endl;
       return -1;
     }
 
   int NbrParticles = ((SingleIntegerOption*) Manager["nbr-particles"])->GetInteger();
   int LzMax = ((SingleIntegerOption*) Manager["lzmax"])->GetInteger();
-  int Lz = ((SingleIntegerOption*) Manager["lz"])->GetInteger();
+  int TotalLz = ((SingleIntegerOption*) Manager["total-lz"])->GetInteger();
   bool FermionFlag = false;
+  bool HaldaneBasisFlag = ((BooleanOption*) Manager["haldane"])->GetBoolean();
+  bool SymmetrizedBasis = ((BooleanOption*) Manager["symmetrized-basis"])->GetBoolean();
   if (((SingleStringOption*) Manager["statistics"])->GetString() == 0)
     FermionFlag = true;
-  if (QHEOnSphereFindSystemInfoFromVectorFileName(((SingleStringOption*) Manager["state"])->GetString(), NbrParticles, LzMax, Lz, FermionFlag) == false)
+  if (QHEOnSphereFindSystemInfoFromVectorFileName(((SingleStringOption*) Manager["state"])->GetString(), NbrParticles, LzMax, TotalLz, FermionFlag) == false)
     {
       return -1;
     }
@@ -102,7 +119,7 @@ int main(int argc, char** argv)
 	{
 	  cout << ((SingleStringOption*) Manager["statistics"])->GetString() << " is an undefined statistics" << endl;
 	}  
-  int Parity = Lz & 1;
+  int Parity = TotalLz & 1;
   if (Parity != ((NbrParticles * LzMax) & 1))
     {
       cout << "Lz and (NbrParticles * LzMax) must have the parity" << endl;
@@ -127,29 +144,123 @@ int main(int argc, char** argv)
   ParticleOnSphere* Space;
   if (FermionFlag == true)
     {
+      if (HaldaneBasisFlag == false)
+	{
 #ifdef __64_BITS__
-      if (LzMax <= 63)
-	{
-	  Space = new FermionOnSphere(NbrParticles, Lz, LzMax, MemorySpace);
-	}
-      else
-	{
-	  Space = new FermionOnSphereUnlimited(NbrParticles, Lz, LzMax, MemorySpace);
-	}
+	  if (LzMax <= 63)
+	    if ((SymmetrizedBasis == false) || (TotalLz != 0))
+	      Space = new FermionOnSphere(NbrParticles, TotalLz, LzMax, MemorySpace);
+	    else
+	      Space = new FermionOnSphereSymmetricBasis(NbrParticles, LzMax, MemorySpace);
+	  else
+	    Space = new FermionOnSphereUnlimited(NbrParticles, TotalLz, LzMax, MemorySpace);
 #else
-      if (LzMax <= 31)
-	{
-	  Space = new FermionOnSphere(NbrParticles, Lz, LzMax, MemorySpace);
+	  if (LzMax <= 31)
+	    if ((SymmetrizedBasis == false) || (TotalLz != 0))
+	      Space = new FermionOnSphere(NbrParticles, TotalLz, LzMax, MemorySpace);
+	    else
+	      Space = new FermionOnSphereSymmetricBasis(NbrParticles, LzMax, MemorySpace);
+	  else
+	    Space = new FermionOnSphereUnlimited(NbrParticles, TotalLz, LzMax, MemorySpace);
+#endif
 	}
       else
 	{
-	  Space = new FermionOnSphereUnlimited(NbrParticles, Lz, LzMax, MemorySpace);
+	  int* ReferenceState = 0;
+	  if (((SingleStringOption*) Manager["reference-file"])->GetString() == 0)
+	    {
+	      ReferenceState = new int[LzMax + 1];
+	      for (int i = 0; i <= LzMax; ++i)
+		ReferenceState[i] = 0;
+	      if (strcasecmp(((SingleStringOption*) Manager["reference-state"])->GetString(), "laughlin") == 0)
+		for (int i = 0; i <= LzMax; i += 3)
+		  ReferenceState[i] = 1;
+	      else
+		if (strcasecmp(((SingleStringOption*) Manager["reference-state"])->GetString(), "pfaffian") == 0)
+		  for (int i = 0; i <= LzMax; i += 4)
+		    {
+		      ReferenceState[i] = 1;
+		      ReferenceState[i + 1] = 1;
+		    }
+		else
+		  if (strcasecmp(((SingleStringOption*) Manager["reference-state"])->GetString(), "readrezayi3") == 0)
+		    for (int i = 0; i <= LzMax; i += 5)
+		      {
+			ReferenceState[i] = 1;
+			ReferenceState[i + 1] = 1;
+			ReferenceState[i + 2] = 1;
+		      }
+		  else
+		    {
+		      cout << "unknown reference state " << ((SingleStringOption*) Manager["reference-state"])->GetString() << endl;
+		      return -1;
+		    }
+	    }
+	  else
+	    {
+	      ConfigurationParser ReferenceStateDefinition;
+	      if (ReferenceStateDefinition.Parse(((SingleStringOption*) Manager["reference-file"])->GetString()) == false)
+		{
+		  ReferenceStateDefinition.DumpErrors(cout) << endl;
+		  return -1;
+		}
+	      if ((ReferenceStateDefinition.GetAsSingleInteger("NbrParticles", NbrParticles) == false) || (NbrParticles <= 0))
+		{
+		  cout << "NbrParticles is not defined or as a wrong value" << endl;
+		  return -1;
+		}
+	      if ((ReferenceStateDefinition.GetAsSingleInteger("LzMax", LzMax) == false) || (LzMax <= 0))
+		{
+		  cout << "LzMax is not defined or as a wrong value" << endl;
+		  return -1;
+		}
+	      int MaxNbrLz;
+	      if (ReferenceStateDefinition.GetAsIntegerArray("ReferenceState", ' ', ReferenceState, MaxNbrLz) == false)
+		{
+		  cout << "error while parsing ReferenceState in " << ((SingleStringOption*) Manager["reference-file"])->GetString() << endl;
+		  return -1;     
+		}
+	      if (MaxNbrLz != (LzMax + 1))
+		{
+		  cout << "wrong LzMax value in ReferenceState" << endl;
+		  return -1;     
+		}
+	    }
+	  if (SymmetrizedBasis == false)
+	    {
+	      if (((SingleStringOption*) Manager["load-hilbert"])->GetString() != 0)
+		Space = new FermionOnSphereHaldaneBasis(((SingleStringOption*) Manager["load-hilbert"])->GetString(), MemorySpace);
+	      else
+		Space = new FermionOnSphereHaldaneBasis(NbrParticles, TotalLz, LzMax, ReferenceState, MemorySpace);
+	      if (((SingleStringOption*) Manager["save-hilbert"])->GetString() != 0)
+		{
+		  ((FermionOnSphereHaldaneBasis*) Space)->WriteHilbertSpace(((SingleStringOption*) Manager["save-hilbert"])->GetString());
+		  return 0;
+		}
+	    }
+	  else
+	    {
+	      if (((SingleStringOption*) Manager["load-hilbert"])->GetString() != 0)
+		Space = new FermionOnSphereHaldaneSymmetricBasis(((SingleStringOption*) Manager["load-hilbert"])->GetString(), MemorySpace);
+	      else
+		Space = new FermionOnSphereHaldaneSymmetricBasis(NbrParticles, LzMax, ReferenceState, MemorySpace);
+	      if (((SingleStringOption*) Manager["save-hilbert"])->GetString() != 0)
+		{
+		  ((FermionOnSphereHaldaneSymmetricBasis*) Space)->WriteHilbertSpace(((SingleStringOption*) Manager["save-hilbert"])->GetString());
+		  return 0;
+		}
+	    }
 	}
-#endif
     }
   else
     {
-      Space = new BosonOnSphere(NbrParticles, Lz, LzMax);
+      Space = new BosonOnSphere(NbrParticles, TotalLz, LzMax);
+    }
+
+  if (Space->GetHilbertSpaceDimension() != State.GetVectorDimension())
+    {
+      cout << "dimension mismatch between the state (" << State.GetVectorDimension() << ") and the Hilbert space (" << Space->GetHilbertSpaceDimension() << ")" << endl;
+      return -1;
     }
 
   int TotalMaxLz = LzMax * NbrParticles;
@@ -158,9 +269,15 @@ int main(int argc, char** argv)
       TotalMaxLz = (LzMax - NbrParticles + 1) * NbrParticles;
     }
 
-   ParticleOnSphereSquareTotalMomentumOperator oper(Space, LzMax);
-   double RawTmpAngularMomentum = 0.5 * (sqrt ((4.0 * oper.MatrixElement(State, State).Re) + 1.0) - 1.0);
-   cout << "<L> = " << RawTmpAngularMomentum << endl;
-   return 0;
+  ParticleOnSphereL2Hamiltonian Hamiltonian (Space, NbrParticles, LzMax, TotalLz, Architecture.GetArchitecture(), 1.0, 0);
+  RealVector TmpState(Space->GetHilbertSpaceDimension());
+  VectorHamiltonianMultiplyOperation Operation (&Hamiltonian, &State, &TmpState);
+  Operation.ApplyOperation(Architecture.GetArchitecture());
+  double L2Value = TmpState * State;
+  double RawTmpAngularMomentum = 0.5 * (sqrt ((4.0 * L2Value) + 1.0) - 1.0);
+  cout << "<L^2> = " << L2Value << endl
+       << "<L> = " << RawTmpAngularMomentum << endl;
+  delete Space;
+  return 0;
 }
 
