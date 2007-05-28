d191c2a4520f15f54de78e8c176bf1ca6011066c
(no author) <(no author)>
2007-05-28 23:06:05 +0000

    A Monte Carlo helper class: implements moving a particle on the sphere in a more physical manner (leading to more accepted moves).
    git-svn-id: https://www.nick-ux.org/diagham/svn/trunk@591 b9410c57-6ef9-0310-98bc-a73ba792996a

---
+++ FQHE/src/Tools/FQHEWaveFunction/ParticleOnSphereCollection.h
@@ -0,0 +1,89 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//                                                                            //
+//                            DiagHam  version 0.01                           //
+//                                                                            //
+//         Copyright (C) 2001-2002 Nicolas Regnault and Gunnar Moeller        //
+//                                                                            //
+//                                                                            //
+//           class of Jain composite fermion wave function on sphere          //
+//                      with filled (pseudo) Landau levels                    //
+//                                                                            //
+//                        last modification : 16/09/2004                      //
+//                                                                            //
+//                                                                            //
+//    This program is free software; you can redistribute it and/or modify    //
+//    it under the terms of the GNU General Public License as published by    //
+//    the Free Software Foundation; either version 2 of the License, or       //
+//    (at your option) any later version.                                     //
+//                                                                            //
+//    This program is distributed in the hope that it will be useful,         //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of          //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           //
+//    GNU General Public License for more details.                            //
+//                                                                            //
+//    You should have received a copy of the GNU General Public License       //
+//    along with this program; if not, write to the Free Software             //
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.               //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef PARTICLEONSPHERECOLLECTION_H
+#define PARTICLEONSPHERECOLLECTION_H
+#include "config.h"
+#include "MathTools/Complex.h"
+#include "MathTools/RandomNumber/NumRecRandomGenerator.h"
+#include "GeneralTools/GarbageFlag.h"
+#include "Vector/RealVector.h"
+
+
+class ParticleOnSphereCollection {
+ private:
+  int NbrParticles;
+  int LastMoved;
+  double Theta0;
+  Complex LastU;
+  Complex LastV;
+  double LastTheta;
+  double LastPhi;
+  Complex  *SpinorUCoordinates;
+  Complex *SpinorVCoordinates;      
+  NumRecRandomGenerator *Generator;
+  GarbageFlag Flag;
+  RealVector ThetaPhi;
+  
+ public:
+
+  // constructors:
+  ParticleOnSphereCollection();
+  ParticleOnSphereCollection(int N, long seed =-1);
+  ParticleOnSphereCollection(const ParticleOnSphereCollection &tocopy);
+
+  //destructor
+  ~ParticleOnSphereCollection();
+  
+   // randomly moves particle number nbrParticle
+  void Move(int nbrParticle);
+
+  // restore last move
+  void RestoreMove();
+
+  // stretch the default steplength by a factor
+  void MultiplyStepLength(double multiplier);
+
+  // access particle positions of a single particle 
+  double Theta(int nbrParticle);
+  double Phi(int nbrParticle);
+
+  // set new particle position
+  void SetPosition(int nbrParticle, double theta, double phi);
+
+  // get all particle positions
+  RealVector& GetPositions();
+
+  
+};
+
+
+#endif
