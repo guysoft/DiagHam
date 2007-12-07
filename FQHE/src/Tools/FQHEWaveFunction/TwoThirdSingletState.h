5b91a81ff23b2cbd9a4b0396e49ec0a848f30654
moller <moller>
2007-12-07 11:40:43 +0000

    Gunnar Möller: Added yet another trial state
    git-svn-id: https://www.nick-ux.org/diagham/svn/trunk@708 b9410c57-6ef9-0310-98bc-a73ba792996a

---
+++ FQHE/src/Tools/FQHEWaveFunction/TwoThirdSingletState.h
@@ -0,0 +1,125 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//                                                                            //
+//                            DiagHam  version 0.01                           //
+//                                                                            //
+//                  Copyright (C) 2007 Gunnar Möller                  //
+//                                                                            //
+//                                                                            //
+//           class of Jain composite fermion wave function on sphere          //
+//                      with filled (pseudo) Landau levels                    //
+//                                                                            //
+//                        last modification : 18/05/2007                      //
+//                                                                            //
+//                                                                            //
+//    This program is free software; you can redistribute it and/or modify    //
+//    it under the terms of the GNU General Public License as published by    //
+//    the Free Software Foundation; either version 2 of the License, or       //
+//    (at your option) any later version.                                     //
+//                                                                            //
+//    This program is distributed in the hope that it will be useful,         //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of          //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           //
+//    GNU General Public License for more details.                            //
+//                                                                            //
+//    You should have received a copy of the GNU General Public License       //
+//    along with this program; if not, write to the Free Software             //
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.               //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef THOTHIRDSINGLETSTATE_H
+#define THOTHIRDSINGLETSTATE_H
+
+
+#include "config.h"
+#include "GeneralTools/GarbageFlag.h"
+#include "MathTools/Complex.h"
+#include "MathTools/NumericalAnalysis/Abstract1DComplexFunction.h"
+#include "Matrix/ComplexMatrix.h"
+#include "Tools/FQHEWaveFunction/JainCFOnSphereOrbitals.h"
+
+class TwoThirdSingletState: public Abstract1DComplexFunction
+{
+
+ protected:
+
+  JainCFOnSphereOrbitals *OrbitalFactory;
+
+  int NbrParticles;
+  int EffectiveFlux;
+
+  GarbageFlag Flag;
+  
+  // factor used to multiply each element of the Slater matrix and Cauchy Permanent
+  double SlaterNorm;
+  double CauchyNorm;
+
+  ComplexMatrix Orbitals;
+  ComplexMatrix *Slater;
+  ComplexMatrix *CauchyPermanent;
+
+  // For internal communication with AdaptNorm:
+  Complex DeterminantValue;
+  Complex PermanentValue;
+
+  // single-particle Jastrow factors
+  Complex **Jij;
+  
+  // if particles are very close to each other, interpolation occurs in JainCFOrbitals
+  // this variable is used to pass on this value between the different subroutines
+  double Interpolation;
+  
+ public:
+
+  // default constructor
+  //
+  TwoThirdSingletState();
+
+  // constructor
+  //
+  // nbrParticles = number of particles
+  TwoThirdSingletState(int nbrParticles);
+
+  // copy constructor
+  //
+  // function = reference on the wave function to copy
+  TwoThirdSingletState(const TwoThirdSingletState& function);
+
+  // destructor
+  //
+  ~TwoThirdSingletState();
+
+  // clone function 
+  //
+  // return value = clone of the function 
+  virtual Abstract1DComplexFunction* Clone ();
+
+  // evaluate function at a given point
+  //
+  // x = point where the function has to be evaluated
+  // return value = function value at x  
+  virtual Complex operator ()(RealVector& x);
+
+  Complex GetTestValue(RealVector& x);
+
+  // normalize the wave-function to one for the given particle positions
+  // x = point where the function has to be evaluated
+  void AdaptNorm(RealVector& x);
+
+  // normalize the wave-function over an average number of MC positions
+  void AdaptAverageMCNorm(int thermalize=100, int average=250);
+
+ private:
+
+  // do all precalculation operations required for a new set of positions
+
+  void EvaluateTables(RealVector& x);
+
+};
+
+
+
+
+#endif //THOTHIRDSINGLETSTATE
