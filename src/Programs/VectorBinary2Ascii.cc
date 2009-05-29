b1a7e3ad9d56e729832b772bf04c18f68e99a94d
regnault <regnault>
2009-05-29 11:01:04 +0000

    Nicolas Regnault: - allowing std output display and tail option in VectorBinary2Ascii
    git-svn-id: https://www.nick-ux.org/diagham/svn/trunk@1144 b9410c57-6ef9-0310-98bc-a73ba792996a

--- src/Programs/VectorBinary2Ascii.cc
+++ src/Programs/VectorBinary2Ascii.cc
@@ -36,9 +36,10 @@ int main(int argc, char** argv)
   (*SystemGroup) += new SingleStringOption  ('i', "input-vector", "name of the file containing the binary vector");
   (*SystemGroup) += new SingleStringOption  ('o', "output-vector", "name of the file where the vector will be stored in ASCII mode");
   (*SystemGroup) += new BooleanOption  ('\n', "add-index", "write ascii vector in a two-column formatted output (first column for the component index, second for the component value)");
-  (*SystemGroup) += new SingleIntegerOption  ('\n', "min-range", "display vector starting from a given component", 0l);
+  (*SystemGroup) += new SingleIntegerOption  ('\n', "min-range", "display vector starting from a given component (is negative, start couting from the last component) from ", 0l);
   (*SystemGroup) += new SingleIntegerOption  ('\n', "max-range", "display vector up to a given component (0 if up to the end)", 0l);
-  
+  (*SystemGroup) += new BooleanOption  ('s', "std-output", "use standard output instead of an output file");
+
   (*MiscGroup) += new BooleanOption  ('h', "help", "display this help");
 
   if (Manager.ProceedOptions(argv, argc, cout) == false)
@@ -63,28 +64,56 @@ int main(int argc, char** argv)
       cout << "can't open vector file " << ((SingleStringOption*) Manager["input-vector"])->GetString() << endl;
       return -1;      
     }
-  if (((SingleStringOption*) Manager["output-vector"])->GetString() == 0)
+  if ((((SingleStringOption*) Manager["output-vector"])->GetString() == 0) && (Manager.GetBoolean("std-output") == false))
     {
       cout << "VectorBinary2Ascii requires an output file" << endl << "see man page for option syntax or type VectorBinary2Ascii -h" << endl;
       return -1;
     }
 
-  ofstream File;
-  File.open(((SingleStringOption*) Manager["output-vector"])->GetString(), ios::out);
-  File.precision(14);
-  long MinValue = 0l;
-  if ((Manager.GetInteger("min-range") > 0l) && (Manager.GetInteger("min-range") < State.GetLargeVectorDimension()))
-    MinValue = Manager.GetInteger("min-range");
-  long MaxValue = State.GetLargeVectorDimension(); 
-  if ((Manager.GetInteger("max-range") < State.GetLargeVectorDimension()) && (Manager.GetInteger("max-range") > MinValue))
-    MaxValue = Manager.GetInteger("max-range");
-  
-  if (((BooleanOption*) Manager["add-index"])->GetBoolean() == true)
-    for (long i = MinValue; i < MaxValue; ++i)
-      File << i << " " << State[i] << endl;
+  if (Manager.GetBoolean("std-output") == false)
+    {
+      ofstream File;
+      File.open(((SingleStringOption*) Manager["output-vector"])->GetString(), ios::out);
+      File.precision(14);
+      long MinValue = 0l;
+      if ((Manager.GetInteger("min-range") > 0l) && (Manager.GetInteger("min-range") < State.GetLargeVectorDimension()))
+	MinValue = Manager.GetInteger("min-range");
+      long MaxValue = State.GetLargeVectorDimension(); 
+      if ((Manager.GetInteger("max-range") < State.GetLargeVectorDimension()) && (Manager.GetInteger("max-range") > MinValue))
+	MaxValue = Manager.GetInteger("max-range");
+      
+      if (((BooleanOption*) Manager["add-index"])->GetBoolean() == true)
+	for (long i = MinValue; i < MaxValue; ++i)
+	  File << i << " " << State[i] << endl;
+      else
+	for (long i = MinValue; i < MaxValue; ++i)
+	  File << State[i] << endl;
+      File.close();
+    }
   else
-    for (long i = MinValue; i < MaxValue; ++i)
-      File << State[i] << endl;
-  File.close();
-  
+    {
+      cout.precision(14);
+      long MinValue = 0l;
+      if (Manager.GetInteger("min-range") > 0l)
+	{
+	  if (Manager.GetInteger("min-range") < State.GetLargeVectorDimension())
+	    MinValue = Manager.GetInteger("min-range");      
+	}
+      else
+	{
+	  long Tmp = State.GetLargeVectorDimension() + Manager.GetInteger("min-range");
+	  if ((Tmp >= 0) && (Tmp < State.GetLargeVectorDimension()))
+	    MinValue = Tmp;
+	}
+      long MaxValue = State.GetLargeVectorDimension();
+      if ((Manager.GetInteger("max-range") < State.GetLargeVectorDimension()) && (Manager.GetInteger("max-range") > MinValue))
+        MaxValue = Manager.GetInteger("max-range");
+      if (Manager.GetBoolean("add-index") == true)
+        for (long i = MinValue; i < MaxValue; ++i)
+          cout << i << " " << State[i] << endl;
+      else
+        for (long i = MinValue; i < MaxValue; ++i)
+          cout << State[i] << endl;
+    }
+  return 0;
 }
