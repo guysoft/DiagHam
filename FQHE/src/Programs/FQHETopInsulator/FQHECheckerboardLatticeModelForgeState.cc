df9f1db492977fd6503a6d57123692d966a64a17
regnault <regnault>
2011-07-01 07:04:12 +0000

    Nicolas Regnault: - minor changes
    git-svn-id: https://www.nick-ux.org/diagham/svn/trunk@1830 b9410c57-6ef9-0310-98bc-a73ba792996a

--- FQHE/src/Programs/FQHETopInsulator/FQHECheckerboardLatticeModelForgeState.cc
+++ FQHE/src/Programs/FQHETopInsulator/FQHECheckerboardLatticeModelForgeState.cc
@@ -54,6 +54,7 @@ int main(int argc, char** argv)
   Manager += MiscGroup;
 
   (*SystemGroup) += new SingleStringOption  ('\0', "state", "name of the vector file describing the state whose density has to be plotted");
+  (*SystemGroup) += new SingleIntegerOption  ('p', "nbr-particles", "number of particles", 2);
   (*SystemGroup) += new SingleIntegerOption  ('x', "nbr-sitex", "number of sites along the x direction", 3);
   (*SystemGroup) += new SingleIntegerOption  ('y', "nbr-sitey", "number of sites along the y direction", 3);
   (*SystemGroup) += new SingleDoubleOption  ('\n', "t1", "nearest neighbor hoping amplitude", 1.0);
@@ -84,34 +85,78 @@ int main(int argc, char** argv)
       return -1;      
     }
 
-  int NbrParticles = 2;
+  int NbrParticles = Manager.GetInteger("nbr-particles"); 
   int NbrSitesX = Manager.GetInteger("nbr-sitex"); 
   int NbrSitesY = Manager.GetInteger("nbr-sitey"); 
+  double NNHoping = Manager.GetDouble("t1");
+  double NextNNHoping = Manager.GetDouble("t2");
+  double SecondNextNNHoping = Manager.GetDouble("tpp");
+  double MuS = Manager.GetDouble("mu-s");
 
   bool Statistics = true;
   
   // ParticleOnCheckerboardLatticeFunctionBasis Basis ();
+
+  ComplexMatrix* OneBodyBasis = new ComplexMatrix [NbrSitesX * NbrSitesY];
+  double GammaX = 0.0;
+  double GammaY = 0.0;
+  for (int kx = 0; kx < NbrSitesX; ++kx)
+    for (int ky = 0; ky < NbrSitesY; ++ky)
+      {
+	int Index = (kx * NbrSitesY) + ky;
+	Complex B1 = 4.0 * NNHoping * Complex (cos (1.0 * M_PI * (((double) kx) + GammaX) / ((double) NbrSitesX)) * cos (1.0 * M_PI * (((double) ky) + GammaY) / ((double) NbrSitesY)) * cos(M_PI * 0.25), 
+						     sin (1.0 * M_PI * (((double) kx) + GammaX) / ((double) NbrSitesX)) * sin (1.0 * M_PI * (((double) ky) + GammaY) / ((double) NbrSitesY)) * sin(M_PI * 0.25));
+	double d1 = 4.0 * SecondNextNNHoping * cos (2.0 * M_PI * (((double) kx) + GammaX) / ((double) NbrSitesX)) * cos (2.0 * M_PI * (((double) ky) + GammaY) / ((double) NbrSitesY));
+	double d3 =  MuS + (2.0 * NextNNHoping * (cos (2.0 * M_PI * (((double) kx) + GammaX) / ((double) NbrSitesX))
+							      - cos (2.0 * M_PI * (((double) ky) + GammaY) / ((double) NbrSitesY))));
+	HermitianMatrix TmpOneBobyHamiltonian(2, true);
+	TmpOneBobyHamiltonian.SetMatrixElement(0, 0, d1 + d3);
+	TmpOneBobyHamiltonian.SetMatrixElement(0, 1, B1);
+	TmpOneBobyHamiltonian.SetMatrixElement(1, 1, d1 - d3);
+	ComplexMatrix TmpMatrix(2, 2, true);
+	TmpMatrix[0][0] = 1.0;
+	TmpMatrix[1][1] = 1.0;
+	RealDiagonalMatrix TmpDiag;
+#ifdef __LAPACK__
+	TmpOneBobyHamiltonian.LapackDiagonalize(TmpDiag, TmpMatrix);
+#else
+	TmpOneBobyHamiltonian.Diagonalize(TmpDiag, TmpMatrix);
+#endif   
+	OneBodyBasis[Index] = TmpMatrix;	
+      }
+
+
   for (int i = 0; i < NbrSitesX; ++i)
     {
       for (int j = 0; j < NbrSitesY; ++j)
 	{
 	  FermionOnSquareLatticeMomentumSpace* Space = new FermionOnSquareLatticeMomentumSpace(NbrParticles, NbrSitesX, NbrSitesY, i, j);
 	  ComplexVector State (Space->GetHilbertSpaceDimension(), true);
-	  for (int kx1 = 0; kx1 < NbrSitesX; ++kx1)
-	    for (int kx2 = 0; kx2 < NbrSitesX; ++kx2)
-	      {
-		if (((kx1 + kx2) % NbrSitesX) == i)
+	  int* MomentumArray = new int [2 * NbrParticles];
+	  if (NbrParticles == 1)
+	    {
+	      MomentumArray[0] = i;
+	      MomentumArray[1] = j;
+	      int Index = Space->FindStateIndexFromArray(MomentumArray);
+	    }
+	  else
+	    {
+	      for (int kx1 = 0; kx1 < NbrSitesX; ++kx1)
+		for (int kx2 = 0; kx2 < NbrSitesX; ++kx2)
 		  {
-		    for (int ky1 = 0; ky1 < NbrSitesY; ++ky1)
-		      for (int ky2 = 0; ky2 < NbrSitesY; ++ky2)
-			{
-			  if (((ky1 + ky2) % NbrSitesY) == j)
+		    if (((kx1 + kx2) % NbrSitesX) == i)
+		      {
+			for (int ky1 = 0; ky1 < NbrSitesY; ++ky1)
+			  for (int ky2 = 0; ky2 < NbrSitesY; ++ky2)
 			    {
-			      
-			    }  
-			}
+			      if (((ky1 + ky2) % NbrSitesY) == j)
+				{
+				  
+				}  
+			    }
+		      }
 		  }
-	      }
+	    }
 	  char* EigenstateOutputFile = new char [512];
 	  if (Manager.GetDouble("mu-s") == 0.0)
 	    sprintf (EigenstateOutputFile, "fermions_singleband_checkerboardlattice_twoparticles_n_%d_x_%d_y_%d_t1_%f_t2_%f_gx_%f_gy_%f_kx_%d_ky_%d.0.vec", NbrParticles, NbrSitesX, NbrSitesY, 
@@ -122,6 +167,7 @@ int main(int argc, char** argv)
 	  State.WriteVector(EigenstateOutputFile);
 	  delete[] EigenstateOutputFile;
 	  delete Space;
+	  delete[] MomentumArray;
 	}
     }
 
