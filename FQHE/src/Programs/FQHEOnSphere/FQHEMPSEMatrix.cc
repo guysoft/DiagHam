d44fe8e095d6695c1ac4c87f319934c402acd7e8
regnault <regnault>
2013-02-15 17:35:52 +0000

    Nicolas Regnault: - remove debugging test
    git-svn-id: https://www.nick-ux.org/diagham/svn/trunk@2493 b9410c57-6ef9-0310-98bc-a73ba792996a

--- FQHE/src/Programs/FQHEOnSphere/FQHEMPSEMatrix.cc
+++ FQHE/src/Programs/FQHEOnSphere/FQHEMPSEMatrix.cc
@@ -154,7 +154,6 @@ int main(int argc, char** argv)
 	}
     }      
   NbrEigenstates += Manager.GetInteger("nbr-excited");
-  NbrEigenstates = 2;
   double EnergyShift = 0.0;
   if (Manager.GetBoolean("power-method") == true)
     {
