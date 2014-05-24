0d1be6c3751a2d285217248db124141350b7871c
regnault <regnault>
2014-05-24 01:25:05 +0000

    Nicolas Regnault: - cleaned version of FQHETorusComputeC4
    git-svn-id: https://www.nick-ux.org/diagham/svn/trunk@2797 b9410c57-6ef9-0310-98bc-a73ba792996a

--- FQHE/src/Programs/FQHEOnTorus/FQHETorusComputeC4.cc
+++ FQHE/src/Programs/FQHEOnTorus/FQHETorusComputeC4.cc
@@ -9,8 +9,6 @@
 #include "HilbertSpace/BosonOnTorusWithMagneticTranslationsShort.h"
 #include "HilbertSpace/FermionOnTorusWithMagneticTranslations.h"
 
-#include "Operator/ParticleOnTorusC4Operator.h"
-
 #include "Architecture/ArchitectureManager.h"
 #include "Architecture/AbstractArchitecture.h"
 #include "Architecture/ArchitectureOperation/VectorOperatorMultiplyOperation.h"
@@ -65,11 +63,10 @@ int main(int argc, char** argv)
 
   (*SystemGroup) += new SingleStringOption('i', "input-state", "name of the file containing the state whose Kx momentum has to be computed");
   (*SystemGroup) += new SingleStringOption('\n', "degenerated-states", "name of the file containing a list of states (override input-state)");
-  (*SystemGroup) += new BooleanOption ('\n',  "compute-eigenstate", "compute the eigenstates of the C4 (or C2) operator in the given basis");
   (*SystemGroup) += new SingleStringOption ('\n',  "interaction-name", "name that should be inserted in the output file names", "dummy");
   (*SystemGroup) += new BooleanOption ('\n',  "apply-c4", "apply the C4 rotation to the input states instead of computing the C4 eigenvalue(s)");
+  (*SystemGroup) += new SingleIntegerOption ('\n',  "target-ky", "force the ky sector of the rotated state, if negative use -kx mod GCD(Ne, Nphi)", -1);
   (*SystemGroup) += new BooleanOption ('\n',  "clockwise", "apply the C4 rotation clockwise");
-  (*SystemGroup) += new BooleanOption ('\n',  "c2-only", "only check the C2 symmetry");
   (*SystemGroup) += new BooleanOption ('\n',  "export-transformation", "export the transformation matrix in a ascii file (one per momentum sector)");
   (*SystemGroup) += new BooleanOption ('\n',  "export-bintransformation", "export the transformation matrix in a binary file (one per momentum sector)");
   (*MiscGroup) += new BooleanOption ('h', "help", "display this help");
@@ -88,8 +85,9 @@ int main(int argc, char** argv)
   int NbrParticles = 0;
   int MaxMomentum = 0;
   int YMomentum = -1;
+  int MomentumModulo = -1;
+  int* YMomenta = 0;
   bool Statistics = true;
-  bool C2OnlyFlag = Manager.GetBoolean("c2-only");
   int XMomentum = -1;
   ComplexVector* InputStates = 0;
   int NbrInputStates = 0;
@@ -115,6 +113,9 @@ int main(int argc, char** argv)
 	}
       NbrInputStates = 1;
       InputStates = new ComplexVector [NbrInputStates];
+      YMomenta = new int [NbrInputStates];
+      YMomenta[0] = YMomentum;
+      MomentumModulo = FindGCD(NbrParticles, MaxMomentum);
       if (InputStates[0].ReadVector(Manager.GetString("input-state")) == false)
 	{
 	  cout << "error while reading " << Manager.GetString("input-state") << endl;
@@ -135,6 +136,7 @@ int main(int argc, char** argv)
 	  cout << "no state found in " << Manager.GetString("degenerated-states") << endl;
 	}
       InputStates = new ComplexVector [NbrInputStates];
+      YMomenta = new int [NbrInputStates];
       if (FQHEOnTorusFindSystemInfoFromVectorFileName(DegeneratedFile(0, 0),
 						      NbrParticles, MaxMomentum, XMomentum, YMomentum, Statistics) == false)
 	{
@@ -142,7 +144,8 @@ int main(int argc, char** argv)
 	  return -1;
 	}
       cout << "Nbr particles=" << NbrParticles << ", Nbr flux quanta=" << MaxMomentum << " Kx=" << XMomentum << " " << " Ky=" << YMomentum << endl;
-      if ((Manager.GetBoolean("apply-c4") == false) && ((XMomentum != YMomentum) || (!((XMomentum == 0) || (((NbrParticles & 1) == 0) && (XMomentum == (NbrParticles / 2)))))))
+      MomentumModulo = FindGCD(NbrParticles, MaxMomentum);
+      if ((Manager.GetBoolean("apply-c4") == false) && ((XMomentum != (YMomentum % MomentumModulo)) || (!((XMomentum == 0) || (((NbrParticles & 1) == 0) && (XMomentum == (NbrParticles / 2)))))))
 	{
 	  cout << "C4 symmetry can only be computed in the (0,0) or (pi,pi) sectors" << endl;
 	  return -1;
@@ -152,25 +155,26 @@ int main(int argc, char** argv)
 	  cout << "error while reading " << DegeneratedFile(0, 0) << endl;
 	  return -1;
 	}
+      YMomenta[0] = YMomentum;
       for (int i = 1; i < NbrInputStates; ++i)
 	{
 	  int TmpNbrParticles = 0;
 	  int TmpMaxMomentum = 0;
-	  int TmpYMomentum = -1;
+	  YMomenta[i] = -1;
 	  int TmpXMomentum = -1;
 	  bool TmpStatistics = true;
 	  if (FQHEOnTorusFindSystemInfoFromVectorFileName(DegeneratedFile(0, i),
-							  TmpNbrParticles, TmpMaxMomentum, TmpXMomentum, TmpYMomentum, TmpStatistics) == false)
+							  TmpNbrParticles, TmpMaxMomentum, TmpXMomentum, YMomenta[i], TmpStatistics) == false)
 	    {
 	      cout << "error while retrieving system parameters from file name " << DegeneratedFile(0, i) << endl;
 	      return -1;
 	    }
 	  if ((TmpNbrParticles != NbrParticles) || (TmpMaxMomentum != MaxMomentum) || 
-	      (TmpYMomentum != YMomentum) || (TmpXMomentum != XMomentum) || (Statistics != TmpStatistics))
+	      ((YMomenta[i] % MomentumModulo) != (YMomentum % MomentumModulo)) || (TmpXMomentum != XMomentum) || (Statistics != TmpStatistics))
 	    {
 	      cout << "error, " << DegeneratedFile(0, i) << " has different system parameters than " << DegeneratedFile(0, 0) 
 		   << ", N=" << TmpNbrParticles << "(" << NbrParticles << "), N_phi=" << TmpMaxMomentum << "(" << MaxMomentum 
-		   << "), Kx=" << TmpXMomentum << "(" << XMomentum << "), Ky=" << TmpYMomentum << "(" << YMomentum << ")" << endl;
+		   << "), Kx=" << TmpXMomentum << "(" << XMomentum << "), Ky=" << YMomenta[i] << " mod " << MomentumModulo << " (" << YMomentum << " mod " << MomentumModulo << ")" << endl;
 	    }
 	  if (InputStates[i].ReadVector(DegeneratedFile(0, i)) == false)
 	    {
@@ -183,59 +187,68 @@ int main(int argc, char** argv)
 	    }
 	}
     }
+  int* UsedYMomenta = new int [MaxMomentum];
+  for (int i = 0; i < MaxMomentum; ++i)
+    UsedYMomenta[i] = 0;
+  for (int i = 0; i < NbrInputStates; ++i)
+    UsedYMomenta[YMomenta[i]]++;
+  int MomentumStep = MaxMomentum / MomentumModulo;
+  for (int i = 0; i < MomentumModulo; ++i)
+    {
+      for (int j = 1; j < MomentumStep; ++j)
+	{
+	  if (UsedYMomenta[i + j * MomentumModulo] != UsedYMomenta[i])
+	    {
+	      cout << "Warning, not providing the same number of states in sector " << i << " and " << (i + j * MomentumModulo) << ", C4 eigenvalue calculation might be wrong" << endl;
+	    }
+	}
+    }
   bool PiPiSectorFlag = false;
   if (XMomentum != 0)
     PiPiSectorFlag = true;
-  ParticleOnTorusWithMagneticTranslations* Space = 0;
-  ParticleOnTorusWithMagneticTranslations* OutputSpace = 0;
-  ParticleOnTorus* UnfoldedSpace = 0;
-  ParticleOnTorus* UnfoldedOutputSpace = 0;
-  int MomentumModulo = FindGCD(NbrParticles, MaxMomentum);
+  ParticleOnTorusWithMagneticTranslations** Spaces = new ParticleOnTorusWithMagneticTranslations*[MaxMomentum];
+  ParticleOnTorusWithMagneticTranslations** OutputSpaces = new ParticleOnTorusWithMagneticTranslations*[MaxMomentum];
   int TargetXMomentum = (MaxMomentum - YMomentum) % MomentumModulo;
   int TargetYMomentum  = XMomentum;
   if (Manager.GetBoolean("clockwise"))
     {
       TargetXMomentum = YMomentum & MomentumModulo;
-      TargetYMomentum = (MaxMomentum - XMomentum) % MaxMomentum;
+      TargetYMomentum = (MaxMomentum - XMomentum) % MomentumModulo;
     }
-  if (Statistics == false)
+  if (Manager.GetInteger("target-ky") >= 0)
     {
-      Space = new BosonOnTorusWithMagneticTranslationsShort(NbrParticles, MaxMomentum, XMomentum, YMomentum);
-      if (Manager.GetBoolean("apply-c4"))
-	{
-	  OutputSpace = new BosonOnTorusWithMagneticTranslationsShort(NbrParticles, MaxMomentum, TargetXMomentum, TargetYMomentum);	
-	  UnfoldedSpace = new BosonOnTorusShort(NbrParticles, MaxMomentum, YMomentum);
-	  UnfoldedOutputSpace = new BosonOnTorusShort(NbrParticles, MaxMomentum, XMomentum);
-	}
-      else
-	{
-	  OutputSpace = (ParticleOnTorusWithMagneticTranslations*) Space->Clone();
-	}
-      sprintf (OutputNamePrefix, "bosons_torus_%s_n_%d_2s_%d", Manager.GetString("interaction-name"), NbrParticles, MaxMomentum);
+      TargetYMomentum = Manager.GetInteger("target-ky") % MaxMomentum;
     }
-  else
+  for (int i = 0; i < MaxMomentum; ++i)
     {
-      Space = new FermionOnTorusWithMagneticTranslations(NbrParticles, MaxMomentum, XMomentum, YMomentum);
-      if (Manager.GetBoolean("apply-c4"))
+      int LocalTargetYMomentum = TargetYMomentum + (i / MomentumModulo) * MomentumModulo;
+      if (UsedYMomenta[i] > 0)
 	{
-	  OutputSpace = new FermionOnTorusWithMagneticTranslations(NbrParticles, MaxMomentum, TargetXMomentum, TargetYMomentum);
-	  UnfoldedSpace = new FermionOnTorus(NbrParticles, MaxMomentum, YMomentum);
-	  UnfoldedOutputSpace = new FermionOnTorus(NbrParticles, MaxMomentum, XMomentum);
+	  if (Statistics == false)
+	    {
+	      Spaces[i] = new BosonOnTorusWithMagneticTranslationsShort(NbrParticles, MaxMomentum, XMomentum, i);
+	      OutputSpaces[LocalTargetYMomentum] = new BosonOnTorusWithMagneticTranslationsShort(NbrParticles, MaxMomentum, TargetXMomentum, LocalTargetYMomentum);	
+	      sprintf (OutputNamePrefix, "bosons_torus_%s_n_%d_2s_%d", Manager.GetString("interaction-name"), NbrParticles, MaxMomentum);
+	    }
+	  else
+	    {
+	      Spaces[i] = new FermionOnTorusWithMagneticTranslations(NbrParticles, MaxMomentum, XMomentum, i);
+	      OutputSpaces[LocalTargetYMomentum] = new FermionOnTorusWithMagneticTranslations(NbrParticles, MaxMomentum, TargetXMomentum, LocalTargetYMomentum);
+	      sprintf (OutputNamePrefix, "fermions_torus_%s_n_%d_2s_%d", Manager.GetString("interaction-name"), NbrParticles, MaxMomentum);
+	    }
+	  Architecture.GetArchitecture()->SetDimension(Spaces[i]->GetHilbertSpaceDimension());
 	}
-      else
-	{
-	  OutputSpace = (ParticleOnTorusWithMagneticTranslations*) Space->Clone();
-	}
-      sprintf (OutputNamePrefix, "fermions_torus_%s_n_%d_2s_%d", Manager.GetString("interaction-name"), NbrParticles, MaxMomentum);
-    }
-  if (InputStates[0].GetVectorDimension() != Space->GetHilbertSpaceDimension())
-    {
-      cout << "error: vector and Hilbert-space have unequal dimensions " << InputStates[0].GetVectorDimension() 
-	   << " "<< Space->GetHilbertSpaceDimension() << endl;
-      return -1;
     }
 
-  Architecture.GetArchitecture()->SetDimension(Space->GetHilbertSpaceDimension());
+  for (int i = 0; i < NbrInputStates; ++i)
+    {
+      if (InputStates[i].GetVectorDimension() != Spaces[YMomenta[i]]->GetHilbertSpaceDimension())
+	{
+	  cout << "error: vector and Hilbert-space have unequal dimensions " << InputStates[i].GetVectorDimension() 
+	       << " "<< Spaces[YMomenta[i]]->GetHilbertSpaceDimension() << endl;
+	  return -1;
+	}
+    }
 
 
   char* OutputName = new char [256 + strlen(OutputNamePrefix)];
@@ -245,17 +258,13 @@ int main(int argc, char** argv)
     {
       for (int i = 0; i < NbrInputStates; ++i)
 	{
-	  ComplexVector TmpVector = Space->ConvertFromKxKyBasis(InputStates[i], UnfoldedSpace);
-	  //	  cout << "input vector:" << endl << TmpVector << endl;
-	  ComplexVector TmpVector2 = UnfoldedOutputSpace->C4Rotation(TmpVector, UnfoldedSpace, Manager.GetBoolean("clockwise"), Architecture.GetArchitecture());
-	  //	  cout << "output vector:" << endl << TmpVector2 << endl;
-	  ComplexVector TmpVector3 = OutputSpace->ConvertToKxKyBasis(TmpVector2, UnfoldedOutputSpace);
-	  double TmpNorm = TmpVector3.Norm();
+	  ComplexVector TmpVector = OutputSpaces[TargetYMomentum]->C4Rotation(InputStates[i], Spaces[YMomenta[i]], Manager.GetBoolean("clockwise"), Architecture.GetArchitecture());
+	  double TmpNorm = TmpVector.Norm();
 	  cout << "Norm of rotated state " << i << " : " << TmpNorm << endl;
-	  TmpVector3 /= TmpNorm;
+	  TmpVector /= TmpNorm;
 	  char* VectorOutputName = new char [256 + strlen(OutputNamePrefix)];
 	  sprintf (VectorOutputName, "%s_c4_kx_%d_ky_%d.%d.vec", OutputNamePrefix, TargetXMomentum, TargetYMomentum, i);
-	  if (TmpVector3.WriteVector(VectorOutputName) == false)
+	  if (TmpVector.WriteVector(VectorOutputName) == false)
 	    {
 	      cout << "error, can't write vector " << VectorOutputName << endl;	      
 	    }
@@ -265,15 +274,12 @@ int main(int argc, char** argv)
     }
   else
     {
-      ParticleOnTorusC4Operator C4Operator(Space, PiPiSectorFlag, C2OnlyFlag);
-            
-      ComplexVector TmpVector(Space->GetHilbertSpaceDimension());
       for (int i = 0; i < NbrInputStates; ++i)
 	{
-	  VectorOperatorMultiplyOperation Operation(&C4Operator, &(InputStates[i]), &TmpVector);
-	  Operation.ApplyOperation(Architecture.GetArchitecture());
 	  for (int j = 0; j < NbrInputStates; ++j)
 	    {
+	      ComplexVector TmpVector = OutputSpaces[YMomenta[j]]->C4Rotation(InputStates[i], Spaces[YMomenta[i]], false, Architecture.GetArchitecture());
+	      double TmpNorm = TmpVector.Norm();
 	      C4Rep.SetMatrixElement(j, i, (TmpVector * InputStates[j]));
 	    }
 	}
@@ -282,146 +288,25 @@ int main(int argc, char** argv)
   ofstream File;
   File.open(OutputName, ios::binary | ios::out);
   File.precision(14);
-  if (C2OnlyFlag == false)
-    {
-      File << "# eigenvalue Norm Arg (2 * Arg/pi) round(C4)" << endl;
-    }
-  else
-    {
-      File << "# eigenvalue Norm Arg (2 * Arg/pi) round(C2)" << endl;
-    }
+  File << "# eigenvalue Norm Arg (2 * Arg/pi) round(C4)" << endl;
 
-  if (Manager.GetBoolean("compute-eigenstate") == false)
+  ComplexDiagonalMatrix Eigenvalues(NbrInputStates, true);
+  C4Rep.LapackDiagonalize(Eigenvalues);
+  double Factor = 2.0 /  M_PI;
+  for (int i = 0; i < NbrInputStates; ++i)
     {
-      ComplexDiagonalMatrix Eigenvalues(NbrInputStates, true);
-      C4Rep.LapackDiagonalize(Eigenvalues);
-      double Factor = 2.0 /  M_PI;
-      for (int i = 0; i < NbrInputStates; ++i)
-	{
-	  Complex TmpValue = Eigenvalues[i];
-	  File << TmpValue << " " << Norm(TmpValue) << " " << Arg(TmpValue);
-	  double TmpValue2 = Arg(TmpValue);
-	  if (fabs(TmpValue2) < MACHINE_PRECISION)
-	    TmpValue2 = 0.0;
-	  if (TmpValue2 < 0.0)
-	    TmpValue2 += 2.0 * M_PI;
-	  if (TmpValue2 >= (2.0 * M_PI))
-	    TmpValue2 -= 2.0 * M_PI;
-	  TmpValue2 *= Factor;
-	  File << " " << TmpValue2 << " " << round(TmpValue2) << endl;
-	  if (NbrInputStates == 1)
-	    {
-	      if (C2OnlyFlag == false)
-		{
-		  cout << "C4 = " << round(TmpValue2) << " (" << TmpValue2 << ")" << endl;
-		}
-	      else
-		{
-		  cout << "C2 = " << round(TmpValue2) << " (" << TmpValue2 << ")" << endl;
-		}
-	    }
-	}      
-    }
-  else
-    {
-      ComplexDiagonalMatrix Eigenvalues(NbrInputStates, true);
-      ComplexMatrix Eigenstates(NbrInputStates, NbrInputStates);
-      C4Rep.LapackDiagonalize(Eigenvalues, Eigenstates);
-      double Factor = 2.0 /  M_PI;
-      int* NbrStatePerC4Sector = new int [4];
-      int* C4Values = new int [NbrInputStates] ;
-      for (int i = 0; i < 4; ++i)
-	NbrStatePerC4Sector[i] = 0;
-      for (int i = 0; i < NbrInputStates; ++i)
-	{
-	  Complex TmpValue = Eigenvalues[i];
-	  File << TmpValue << " " << Norm(TmpValue) << " " << Arg(TmpValue);
-	  double TmpValue2 = Arg(TmpValue);
-	  if (fabs(TmpValue2) < MACHINE_PRECISION)
-	    TmpValue2 = 0.0;
-	  if (TmpValue2 < 0.0)
-	    TmpValue2 += 2.0 * M_PI;
-	  if (TmpValue2 >= (2.0 * M_PI))
-	    TmpValue2 -= 2.0 * M_PI;
-	  TmpValue2 *= Factor;
-	  int TmpC4 = round(TmpValue2);
-	  File << " " << TmpValue2 << " " << TmpC4 << endl;
-	  C4Values[i] = TmpC4;
-	  NbrStatePerC4Sector[TmpC4]++;
-	}
-      ComplexVector** TmpVectors = new ComplexVector*[4];
-      for (int i = 0; i < 4; ++i)
-	{
-	  TmpVectors[i]  = new ComplexVector[NbrStatePerC4Sector[i]];
-	  NbrStatePerC4Sector[i] = 0;
-	}
-      ComplexVector TmpVector (Space->GetHilbertSpaceDimension());
-      for (int i = 0; i < NbrInputStates; ++i)
-	{
-	  Complex TmpValue = Eigenvalues[i];
-	  for (int j = 0; j < Space->GetHilbertSpaceDimension(); ++j)
-	    {
-	      Complex Tmp = 0.0;
-	      for (int k = 0; k < NbrInputStates; ++k)
-		{
-		  Tmp += Conj(Eigenstates[i][k]) * InputStates[k][j];
-		}
-	      TmpVector[j] = Tmp;		  
-	    }
-	  NbrStatePerC4Sector[C4Values[i]]++;	      
-	}	  
-      
-      ComplexMatrix* TransformationMatrices = new ComplexMatrix [4]; 
-      for (int i = 0; i < 4; ++i)
-	{
-	  ComplexMatrix TmpMatrix (TmpVectors[i], NbrStatePerC4Sector[i]);
-	  TmpMatrix.OrthoNormalizeColumns(TransformationMatrices[i]);
-	  for (int j = 0; j < NbrStatePerC4Sector[i]; ++j)
-	    {
-	      char* VectorOutputName = new char [256 + strlen(OutputNamePrefix)];
-	      sprintf (VectorOutputName, "%s_c4_%d_kx_%d_ky_%d.%d.vec", OutputNamePrefix, i, XMomentum, YMomentum, j);
-	      if (TmpMatrix[j].WriteVector(VectorOutputName) == false)
-		{
-		  cout << "error, can't write vector " << VectorOutputName << endl;
-		}
-	      delete[] VectorOutputName;	  	      
-	    }
-	}	  
-      if ((Manager.GetBoolean("export-transformation") == true) || 
-	  (Manager.GetBoolean("export-bintransformation") == true))
-	{
-	  for (int i = 0; i < 4; ++i)
-	    {
-	      if (NbrStatePerC4Sector[i] > 0)
-		{
-		  ComplexMatrix TmpMatrix(NbrInputStates, NbrStatePerC4Sector[i]);
-		  int Tmp = 0;
-		  for (int j = 0; j < NbrInputStates; ++j)
-		    {
-		      if (C4Values[j] == i)
-			{
-			  TmpMatrix[Tmp] = Eigenstates[j];
-			  ++Tmp;
-			}
-		    }
-		  TmpMatrix.ComplexConjugate();
-		  TmpMatrix.Multiply(TransformationMatrices[i]);
-		  char* MatrixOutputName =  new char [256 + strlen(OutputNamePrefix)];
-		  if (Manager.GetBoolean("export-transformation") == true)
-		    {
-		      sprintf (MatrixOutputName, "%s_c4_%d_kx_%d_ky_%d.mat.txt", OutputNamePrefix, i, XMomentum, YMomentum);
-		      TmpMatrix.WriteAsciiMatrix(MatrixOutputName);
-		    }
-		  else
-		    {
-		      sprintf (MatrixOutputName, "%s_c4_%d_kx_%d_ky_%d.mat", OutputNamePrefix, i, XMomentum, YMomentum);
-		      TmpMatrix.WriteMatrix(MatrixOutputName);
-		    }
-		  delete[] MatrixOutputName;
-		}
-	    }
-	}
-      delete[] TmpVectors;
+      Complex TmpValue = Eigenvalues[i];
+      File << TmpValue << " " << Norm(TmpValue) << " " << Arg(TmpValue);
+      double TmpValue2 = Arg(TmpValue);
+      if (fabs(TmpValue2) < MACHINE_PRECISION)
+	TmpValue2 = 0.0;
+      if (TmpValue2 < 0.0)
+	TmpValue2 += 2.0 * M_PI;
+      if (TmpValue2 >= (2.0 * M_PI))
+	TmpValue2 -= 2.0 * M_PI;
+      TmpValue2 *= Factor;
+      File << " " << TmpValue2 << " " << round(TmpValue2) << endl;
+      cout << "C4 = " << round(TmpValue2) << " (" << TmpValue2 << ")" << endl;
     }
   File.close();
   return 0;
