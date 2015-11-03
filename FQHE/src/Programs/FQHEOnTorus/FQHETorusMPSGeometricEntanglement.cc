ce412d3094577a51c55f9797d9658f0a0fa5f6bb
sterdyni <sterdyni>
2015-11-03 16:30:16 +0000

    antoine: - adding  grouping orbitals for geometric entanglement
    git-svn-id: https://www.nick-ux.org/diagham/svn/trunk@3244 b9410c57-6ef9-0310-98bc-a73ba792996a

--- FQHE/src/Programs/FQHEOnTorus/FQHETorusMPSGeometricEntanglement.cc
+++ FQHE/src/Programs/FQHEOnTorus/FQHETorusMPSGeometricEntanglement.cc
@@ -72,6 +72,7 @@ int main(int argc, char** argv)
   Manager += MiscGroup;
 
   (*SystemGroup) += new SingleIntegerOption ('\n', "nbr-fluxquanta", "set the total number of flux quanta and deduce the number of particles", 0);
+  (*SystemGroup) += new SingleIntegerOption ('\n', "nbr-fusedblock", "number of blocks to be considered simultaneously", 1);
   (*SystemGroup) += new SingleIntegerOption ('\n', "topologicalsector", "set the topological sector of state", 0);
   (*MiscGroup) += new BooleanOption  ('h', "help", "display this help");  
 
@@ -94,7 +95,8 @@ int main(int argc, char** argv)
       cout << "invalid number of flux quanta" << endl;
       return -1;
     }
-   NbrFluxQuanta = Manager.GetInteger("nbr-fluxquanta");
+  NbrFluxQuanta = Manager.GetInteger("nbr-fluxquanta");
+  int SizeBlock =  Manager.GetInteger("nbr-fusedblock");
 
   AbstractFQHEMPSMatrix* MPSMatrix = MPSMatrixManager.GetMPSMatrices(NbrFluxQuanta, Architecture.GetArchitecture()); 
 
@@ -103,17 +105,52 @@ int main(int argc, char** argv)
       return 0;
     }
 
-  int NbrBMatrices = MPSMatrix->GetNbrMatrices();
-  cout << "handling " << NbrBMatrices << " B matrices" << endl;
   int NbrParticles = MPSMatrix->GetMatrixNaturalNbrParticles(NbrFluxQuanta, true);
-  cout << "Nbr of particles = " << NbrParticles << " " << ", Nbr of flux quanta=" << NbrFluxQuanta << endl;
+  int NbrBlock =  NbrParticles/SizeBlock;
+  if ( (NbrParticles  % NbrBlock) != 0)
+    {
+      cout << "invalid number of flux quanta" << endl;
+      return -1;
+    }
+
+  int NbrBMatrices = MPSMatrix->GetNbrMatrices();
+
+  int NbrOrbitals = MPSMatrix->GetNbrOrbitals();
+  int NbrStatesPerOrbital = MPSMatrix->GetMaximumOccupation() + 1;
+  cout << " MPSMatrix->GetMaximumOccupation()  = "<< MPSMatrix->GetMaximumOccupation()<<endl;
+  cout <<"NbrOrbitals = "<< NbrOrbitals<<endl;
+  int DimensionPhysicalHilbertSpace = 1;
+
+  for (int i = 1; i < NbrOrbitals * SizeBlock; i++)
+     DimensionPhysicalHilbertSpace *= NbrStatesPerOrbital;
+
+  cout << "handling " << NbrBMatrices << " B matrices" << endl;
+  cout << Nbr of particles = " << NbrParticles << " " << ", Nbr of flux quanta=" << NbrFluxQuanta << endl;
+  cout <<"Size block = "<< SizeBlock<< " , Nbr blocks = " << NbrBlock<<endl;
+  cout <<"Dimension of the physical Hilbert space = " << DimensionPhysicalHilbertSpace <<endl;
+
   SparseRealMatrix* BMatrices = MPSMatrix->GetMatrices();
 
   cout << "B matrix size = " << BMatrices[0].GetNbrRow() << "x" << BMatrices[0].GetNbrColumn() << endl;
   
-   RealVector CoefficientVector (NbrBMatrices,true);
+  SparseRealMatrix* FusedBMatrices = new SparseRealMatrix [DimensionPhysicalHilbertSpace];
+
+  int TmpI;
+  for(int i =0 ; i <DimensionPhysicalHilbertSpace; i++)
+  { 
+     TmpI = i,			
+     FusedBMatrices[i].Copy(BMatrices[TmpI %  NbrStatesPerOrbital]);
+     TmpI /=NbrStatesPerOrbital;
+     for(int p = 1; p < SizeBlock - 1; p++)
+	{
+           FusedBMatrices[i].Multiply(BMatrices[TmpI %  NbrStatesPerOrbital]);
+           TmpI /=NbrStatesPerOrbital;
+	}
+  }
+
+   RealVector CoefficientVector (DimensionPhysicalHilbertSpace,true);
  
-   for (int i = 0; i <NbrBMatrices; i++)
+   for (int i = 0; i <DimensionPhysicalHilbertSpace; i++)
    {
       CoefficientVector[i] = ((double) rand() / (RAND_MAX) - 0.5);
    }
@@ -123,26 +160,27 @@ double lamba = 0.0;
 double Newlamba = 100.0;
 
  cout <<"Start algorithm"<<endl;
- while ( fabs(lamba - Newlamba) > 1e-4  ) 
-{
-  SparseRealMatrix TmpMatrix =  BMatrices[0] * CoefficientVector[0];
 
-  for (int i = 1; i<NbrBMatrices; i++)
+ while ( fabs(lamba - Newlamba) > 1e-8  ) 
+{
+  SparseRealMatrix TmpMatrix =  FusedBMatrices[0] * CoefficientVector[0];
+
+  for (int i = 1; i<DimensionPhysicalHilbertSpace; i++)
   {
-      TmpMatrix = TmpMatrix + (BMatrices[i] * CoefficientVector[i]);
+      TmpMatrix = TmpMatrix + (FusedBMatrices[i] * CoefficientVector[i]);
   }
  
 
   SparseRealMatrix TmpMatrix2;
   TmpMatrix2.Copy(TmpMatrix);
-  for (int i = 1; i< NbrParticles - 1; i++)
+  for (int i = 1; i< NbrBlock - 1; i++)
   {
       TmpMatrix2.Multiply(TmpMatrix);
   }
 
-  for (int i = 0; i < NbrBMatrices; i++)
+  for (int i = 0; i < DimensionPhysicalHilbertSpace; i++)
  {
-   SparseRealMatrix TmpMatrix3 = Multiply(BMatrices[i],TmpMatrix2);
+   SparseRealMatrix TmpMatrix3 = Multiply(FusedBMatrices[i],TmpMatrix2);
    CoefficientVector[i] = TmpMatrix3.Tr();
  }
   lamba = Newlamba ;
@@ -153,17 +191,17 @@ double Newlamba = 100.0;
 }
 
 
-  SparseRealMatrix TmpMatrix =  BMatrices[0] * CoefficientVector[0];
+  SparseRealMatrix TmpMatrix =  FusedBMatrices[0] * CoefficientVector[0];
 
-  for (int i = 1; i<NbrBMatrices; i++)
+  for (int i = 1; i<DimensionPhysicalHilbertSpace; i++)
   {
-      TmpMatrix = TmpMatrix + (BMatrices[i] * CoefficientVector[i]);
+      TmpMatrix = TmpMatrix + (FusedBMatrices[i] * CoefficientVector[i]);
   }
  
 
   SparseRealMatrix TmpMatrix2;
   TmpMatrix2.Copy(TmpMatrix);
-  for (int i = 1; i < NbrParticles ; i++)
+  for (int i = 1; i < NbrBlock ; i++)
   {
      TmpMatrix2.Multiply(TmpMatrix);
   }
@@ -171,10 +209,9 @@ double Newlamba = 100.0;
  double FinalResult = TmpMatrix2.Tr();
 
  cout <<" FinalResult = "<<FinalResult <<endl;
- unsigned long *  PhysicalIndice = MPSMatrix->GetPhysicalIndices();
 
- for (int i = 0; i < NbrBMatrices;  i++)
-   cout << PhysicalIndice[i] <<" " <<CoefficientVector[i]<<endl;
+ for (int i = 0; i < DimensionPhysicalHilbertSpace;  i++)
+   cout << i <<" " <<CoefficientVector[i]<<endl;
 
   return 0;
 }
