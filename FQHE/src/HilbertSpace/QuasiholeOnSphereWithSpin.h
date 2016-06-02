7e2bc34804e1cf9862e317d0df0a8725883d7a63
regnault <regnault>
2016-06-02 23:08:09 +0000

    Nicolas Regnault: - some additional cleanup
    git-svn-id: https://www.nick-ux.org/diagham/svn/trunk@3422 b9410c57-6ef9-0310-98bc-a73ba792996a

--- FQHE/src/HilbertSpace/QuasiholeOnSphereWithSpin.h
+++ FQHE/src/HilbertSpace/QuasiholeOnSphereWithSpin.h
@@ -111,24 +111,6 @@ class QuasiholeOnSphereWithSpin :  public QuasiholeOnSphereWithSpinAndPairing
   // return value = number of left states that are connected to the initial state
   virtual int AduAdd (int index, int m, int*& leftIndices, double*& interactionElements);
   
-  // apply a^\dagger_u_m a_u_m to a given state
-  //
-  // index = index of the state on which the operator has to be applied
-  // m = index for destruction operators
-  // leftIndices = reference to an array containing the indices of the resulting states
-  // interactionElements = reference to an array containing the matrix elements 
-  // return value = number of left states that are connected to the initial state
-  virtual int AduAu (int index, int m, int*& leftIndices, double*& interactionElements);
-  
-  // apply a^\dagger_d_m a_d_m to a given state
-  //
-  // index = index of the state on which the operator has to be applied
-  // m = index for destruction operators
-  // leftIndices = reference to an array containing the indices of the resulting states
-  // interactionElements = reference to an array containing the matrix elements 
-  // return value = number of left states that are connected to the initial state
-  virtual int AddAd (int index, int m, int*& leftIndices, double*& interactionElements);
-  
   // get the number of particles in a given state
   //
   // index =index of the state whose number of particles has to be returned
