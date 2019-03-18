52c3070f0dd989543368f5f38716d32505ef5ffc
regnault <regnault>
2019-03-18 09:38:29 +0000

    Nicolas Regnault: - add support for vectors stored as a matrix in GenericOverlap
    git-svn-id: https://www.nick-ux.org/diagham/svn/trunk@4031 b9410c57-6ef9-0310-98bc-a73ba792996a

--- src/Programs/GenericOverlap.cc
+++ src/Programs/GenericOverlap.cc
@@ -35,6 +35,7 @@ int main(int argc, char** argv)
 
   (*SystemGroup) += new MultipleStringOption  ('\0', "states", "names of the vector files obtained using exact diagonalization");
   (*SystemGroup) += new SingleStringOption  ('\n', "state-list", "provide the names of the vector using a single column formatted text file");
+  (*SystemGroup) += new SingleStringOption  ('\n', "states-matrix", "use a series of vectors stored as c");
   
   (*SystemGroup) += new BooleanOption  ('c', "complex", "Assume vectors consist of complex numbers");
   (*SystemGroup) += new BooleanOption  ('s', "scalar-product", "Get the scalar product, not the overlap");
@@ -109,236 +110,470 @@ int main(int argc, char** argv)
 	}
     }
 
-  if (NbrVectors < 2)
+  if ((NbrVectors < 2) && (Manager.GetString("states-matrix")) == 0)
     {
       cout << "At least two vector files are required!"<<endl;
       exit(1);
     }
 
   if (QuietFlag == false)
-    for (int i=0; i<NbrVectors; ++i)    
+    for (int i = 0; i < NbrVectors; ++i)    
       cout << "File "<<i<<"  "<<VectorFiles[i]<<endl;
 
 
   int MaxVectors= (Manager.GetBoolean("no-cross") ? 1:NbrVectors);
 
   bool HaveComplex = Manager.GetBoolean("complex");
-  
-  if (HaveComplex)
-    {      
-      Complex sp = 0.0;
-      Complex TotalOverlap = 0.0;
-      ComplexVector State1, State2;
-      ComplexMatrix OverlapMatrix;
-      if (MaxVectors == 1) 
-	OverlapMatrix = ComplexMatrix(MaxVectors, NbrVectors - 1, true);
-      else
-	OverlapMatrix = ComplexMatrix(MaxVectors, NbrVectors, true);
-      for (int i=0; i < MaxVectors; ++i)
-	{
-	  if (State1.ReadVector (VectorFiles[i]) == false)
+
+
+  if (Manager.GetString("states-matrix") == 0)
+    {
+      if (HaveComplex)
+	{      
+	  Complex sp = 0.0;
+	  Complex TotalOverlap = 0.0;
+	  ComplexVector State1, State2;
+	  ComplexMatrix OverlapMatrix;
+	  if (MaxVectors == 1) 
+	    OverlapMatrix = ComplexMatrix(MaxVectors, NbrVectors - 1, true);
+	  else
+	    OverlapMatrix = ComplexMatrix(MaxVectors, NbrVectors, true);
+	  for (int i=0; i < MaxVectors; ++i)
 	    {
-	      cout << "can't open vector file " << VectorFiles[i] << endl;
-	      return -1;      
-	    }
-	  if ((i==0)&&(Manager.GetBoolean("dimension")))
-	    {
-	      cout << "Vector dimension = "<<State1.GetVectorDimension() <<endl;
-	    }
-	  if (Manager.GetBoolean("normalize"))
-	    State1 /= State1.Norm();
-	  for (int j= i + 1; j < NbrVectors; ++j)
-	    {	      
-	      if (State2.ReadVector (VectorFiles[j]) == false)
+	      if (State1.ReadVector (VectorFiles[i]) == false)
 		{
-		  cout << "can't open vector file " << VectorFiles[j] << endl;
+		  cout << "can't open vector file " << VectorFiles[i] << endl;
 		  return -1;      
 		}
-	      if (State1.GetVectorDimension() != State2.GetVectorDimension() )
+	      if ((i==0)&&(Manager.GetBoolean("dimension")))
 		{
-		  cout << "Dimension of Hilbert spaces in input files does not coincide" << endl;
-		  return -2;
+		  cout << "Vector dimension = "<<State1.GetVectorDimension() <<endl;
 		}
 	      if (Manager.GetBoolean("normalize"))
-		State2 /= State2.Norm();
-	      sp = 0.0;
-	      if (Manager.GetBoolean("discard-sign"))
-		for (int k = 0; k < State1.GetVectorDimension(); ++k)
-		  sp += Norm(State1[k] * State2[k]);
-	      else
-		if (Manager.GetBoolean("conjugate"))
-		  sp = EuclidianScalarProduct(State1, State2);
-		else
-		  sp = State1 * State2;
-	      if (Scalar == false)
-		{
-		  TotalOverlap += SqrNorm(sp);
-		}
-	      else
-		{
-		  TotalOverlap += sp;
-		}
-	      if (MaxVectors == 1)
-		{
-		  OverlapMatrix.SetMatrixElement(0, j - 1, sp);
-		}
-	      else
-		{
-		  OverlapMatrix.SetMatrixElement(j, i, sp);
-		  OverlapMatrix.SetMatrixElement(i, j, sp);
-		  OverlapMatrix.SetMatrixElement(i, i, 1.0);
-		}
-	      if (Scalar == false)
-		{
-		  if (QuietFlag == false)
+		State1 /= State1.Norm();
+	      for (int j= i + 1; j < NbrVectors; ++j)
+		{	      
+		  if (State2.ReadVector (VectorFiles[j]) == false)
 		    {
-		      if (SqrNorm(sp)>=Threshold)
-			cout << "Overlap |<"<<i<<"|"<<j<<">|^2 = " << SqrNorm(sp) << endl;
+		      cout << "can't open vector file " << VectorFiles[j] << endl;
+		      return -1;      
+		    }
+		  if (State1.GetVectorDimension() != State2.GetVectorDimension() )
+		    {
+		      cout << "Dimension of Hilbert spaces in input files does not coincide" << endl;
+		      return -2;
+		    }
+		  if (Manager.GetBoolean("normalize"))
+		    State2 /= State2.Norm();
+		  sp = 0.0;
+		  if (Manager.GetBoolean("discard-sign"))
+		    for (int k = 0; k < State1.GetVectorDimension(); ++k)
+		      sp += Norm(State1[k] * State2[k]);
+		  else
+		    if (Manager.GetBoolean("conjugate"))
+		      sp = EuclidianScalarProduct(State1, State2);
+		    else
+		      sp = State1 * State2;
+		  if (Scalar == false)
+		    {
+		      TotalOverlap += SqrNorm(sp);
 		    }
 		  else
-		    cout << SqrNorm(sp) << endl;
+		    {
+		      TotalOverlap += sp;
+		    }
+		  if (MaxVectors == 1)
+		    {
+		      OverlapMatrix.SetMatrixElement(0, j - 1, sp);
+		    }
+		  else
+		    {
+		      OverlapMatrix.SetMatrixElement(j, i, sp);
+		      OverlapMatrix.SetMatrixElement(i, j, sp);
+		      OverlapMatrix.SetMatrixElement(i, i, 1.0);
+		    }
+		  if (Scalar == false)
+		    {
+		      if (QuietFlag == false)
+			{
+			  if (SqrNorm(sp)>=Threshold)
+			    cout << "Overlap |<"<<i<<"|"<<j<<">|^2 = " << SqrNorm(sp) << endl;
+			}
+		      else
+			cout << SqrNorm(sp) << endl;
+		    }
+		  else
+		    {
+		      if (QuietFlag == false)
+			{
+			  if (Norm(sp)>=Threshold)
+			    {
+			      if (Manager.GetBoolean("polar"))
+				cout << "<"<<i<<"|"<<j<<"> = " << Norm(sp)<<"*Exp("<< Arg(sp)/M_PI<< " I Pi)" << endl;
+			      else
+				cout << "<"<<i<<"|"<<j<<"> = " << sp << endl;
+			    }
+			}
+		      else
+			cout << sp.Re << " " << sp.Im << endl;
+		    }
+		}
+	    }
+	  if (Manager.GetBoolean("sum") == true)
+	    {
+	      if (Scalar == false)
+		{
+		  if (QuietFlag == false)
+		    cout << "Total overlap = " << TotalOverlap.Re << endl;
+		  else
+		    cout << TotalOverlap.Re << endl;
 		}
 	      else
 		{
 		  if (QuietFlag == false)
+		    cout << "Total overlap = " << TotalOverlap << endl;
+		  else
+		    cout << TotalOverlap << endl;
+		}
+	    }
+	  if (Manager.GetString("matrix") != 0)
+	    {
+	      OverlapMatrix.WriteMatrix(Manager.GetString("matrix"));
+	    }
+	}
+      else // real vectors
+	{
+	  double TotalOverlap = 0.0;
+	  double sp = 0.0;
+	  RealVector State1, State2;
+	  RealMatrix OverlapMatrix;
+	  if (MaxVectors == 1) 
+	    OverlapMatrix = RealMatrix(MaxVectors, NbrVectors - 1, true);
+	  else
+	    OverlapMatrix = RealMatrix(MaxVectors, NbrVectors, true);
+	  for (int i = 0; i < MaxVectors; ++i)
+	    {
+	      if (State1.ReadVector (VectorFiles[i]) == false)
+		{
+		  cout << "can't open vector file " << VectorFiles[i] << endl;
+		  return -1;      
+		}
+	      if ((i==0)&&(Manager.GetBoolean("dimension")))
+		{
+		  cout << "Vector dimension = "<<State1.GetVectorDimension() <<endl;
+		}
+	      if (Manager.GetBoolean("normalize"))
+		State1 /= State1.Norm();
+	      for (int j = i + 1; j < NbrVectors; ++j)
+		{	      
+		  if (State2.ReadVector (VectorFiles[j]) == false)
 		    {
-		      if (Norm(sp)>=Threshold)
+		      cout << "can't open vector file " << VectorFiles[j] << endl;
+		      return -1;      
+		    }
+		  if (State1.GetVectorDimension() != State2.GetVectorDimension() )
+		    {
+		      cout << "Dimension of Hilbert spaces in input files does not coincide" << endl;
+		      return -2;
+		    }
+		  if (Manager.GetBoolean("normalize"))
+		    State2/=State2.Norm();
+		  sp = 0.0;
+		  if (Manager.GetBoolean("discard-sign"))
+		    {
+		      for (int k = 0; k < State1.GetVectorDimension(); ++k)
+			sp += fabs(State1[k] * State2[k]);
+		    }
+		  else
+		    {
+		      sp = State1 * State2 ;
+		    }
+		  if (MaxVectors == 1)
+		    {
+		      OverlapMatrix.SetMatrixElement(0, j - 1, sp);
+		    }
+		  else
+		    {
+		      OverlapMatrix.SetMatrixElement(j, i, sp);
+		      OverlapMatrix.SetMatrixElement(i, j, sp);
+		      OverlapMatrix.SetMatrixElement(i, i, 1.0);
+		    }
+		  if (Scalar == false)
+		    {
+		      TotalOverlap += SqrNorm(sp);
+		    }
+		  else
+		    {
+		      TotalOverlap += sp;
+		    }
+		  if (Scalar == false)
+		    {
+		      if (QuietFlag == false)
 			{
-			  if (Manager.GetBoolean("polar"))
-			    cout << "<"<<i<<"|"<<j<<"> = " << Norm(sp)<<"*Exp("<< Arg(sp)/M_PI<< " I Pi)" << endl;
-			  else
-			    cout << "<"<<i<<"|"<<j<<"> = " << sp << endl;
+			  if (SqrNorm(sp) >= Threshold)
+			    cout << "Overlap |<"<<i<<"|"<<j<<">|^2 = " << SqrNorm(sp) << endl;
+			}
+		      else
+			{
+			  cout << SqrNorm(sp) << endl;
 			}
 		    }
 		  else
-		    cout << sp.Re << " " << sp.Im << endl;
+		    {
+		      if (QuietFlag == false)
+			{
+			  if (fabs(sp)>=Threshold)
+			    cout << "<"<<i<<"|"<<j<<"> = " << sp << endl;
+			}
+		      else
+			{
+			  cout << sp << endl;
+			}
+		    }
+		  
 		}
 	    }
-	}
-      if (Manager.GetBoolean("sum") == true)
-	{
-	  if (Scalar == false)
-	    {
-	      if (QuietFlag == false)
-		cout << "Total overlap = " << TotalOverlap.Re << endl;
-	      else
-		cout << TotalOverlap.Re << endl;
-	    }
-	  else
+	  if (Manager.GetBoolean("sum") == true)
 	    {
 	      if (QuietFlag == false)
 		cout << "Total overlap = " << TotalOverlap << endl;
 	      else
 		cout << TotalOverlap << endl;
 	    }
-	}
-      if (Manager.GetString("matrix") != 0)
-	{
-	  OverlapMatrix.WriteMatrix(Manager.GetString("matrix"));
+	  if (Manager.GetString("matrix") != 0)
+	    {
+	      OverlapMatrix.WriteMatrix(Manager.GetString("matrix"));
+	    }
 	}
     }
-  else // real vectors
+  else
     {
-      double TotalOverlap = 0.0;
-      double sp = 0.0;
-      RealVector State1, State2;
-      RealMatrix OverlapMatrix;
-      if (MaxVectors == 1) 
-	OverlapMatrix = RealMatrix(MaxVectors, NbrVectors - 1, true);
-      else
-	OverlapMatrix = RealMatrix(MaxVectors, NbrVectors, true);
-      for (int i = 0; i < MaxVectors; ++i)
+      if (HaveComplex)
 	{
-	  if (State1.ReadVector (VectorFiles[i]) == false)
+	  ComplexMatrix VectorsAsAMatrix;
+	  if (VectorsAsAMatrix.ReadMatrix(Manager.GetString("states-matrix")) == false)
 	    {
-	      cout << "can't open vector file " << VectorFiles[i] << endl;
-	      return -1;      
+	      cout << "error while reading matrix " << Manager.GetString("states-matrix") << endl;
+	      return -1;
 	    }
-	  if ((i==0)&&(Manager.GetBoolean("dimension")))
+	  ComplexVector* StateVectors = new ComplexVector [NbrVectors + VectorsAsAMatrix.GetNbrColumn()];
+	  for (int i = 0; i < NbrVectors; ++i)
 	    {
-	      cout << "Vector dimension = "<<State1.GetVectorDimension() <<endl;
+	      if (StateVectors[i].ReadVector (VectorFiles[i]) == false)
+		{
+		  cout << "error while reading vector " << Manager.GetString(VectorFiles[i]) << endl;
+		  return -1;		  
+		}
+	      if (StateVectors[i].GetVectorDimension() != VectorsAsAMatrix.GetNbrRow())
+		{
+		  cout << "dimension mismacth bewteen vector " << Manager.GetString(VectorFiles[i]) << " (" << StateVectors[i].GetVectorDimension() << ") and "
+		       << Manager.GetString("states-matrix") << " (" << VectorsAsAMatrix.GetNbrRow() << ")" << endl;		  
+		}
 	    }
-	  if (Manager.GetBoolean("normalize"))
-	    State1 /= State1.Norm();
-	  for (int j = i + 1; j < NbrVectors; ++j)
-	    {	      
-	      if (State2.ReadVector (VectorFiles[j]) == false)
-		{
-		  cout << "can't open vector file " << VectorFiles[j] << endl;
-		  return -1;      
-		}
-	      if (State1.GetVectorDimension() != State2.GetVectorDimension() )
-		{
-		  cout << "Dimension of Hilbert spaces in input files does not coincide" << endl;
-		  return -2;
-		}
-	      if (Manager.GetBoolean("normalize"))
-		State2/=State2.Norm();
-	      sp = 0.0;
-	      if (Manager.GetBoolean("discard-sign"))
-		{
-		  for (int k = 0; k < State1.GetVectorDimension(); ++k)
-		    sp += fabs(State1[k] * State2[k]);
-		}
-	      else
-		{
-		  sp = State1 * State2 ;
-		}
-	      if (MaxVectors == 1)
-		{
-		  OverlapMatrix.SetMatrixElement(0, j - 1, sp);
-		}
-	      else
-		{
-		  OverlapMatrix.SetMatrixElement(j, i, sp);
-		  OverlapMatrix.SetMatrixElement(i, j, sp);
-		  OverlapMatrix.SetMatrixElement(i, i, 1.0);
-		}
-	      if (Scalar == false)
-		{
-		  TotalOverlap += SqrNorm(sp);
-		}
-	      else
-		{
-		  TotalOverlap += sp;
-		}
-	      if (Scalar == false)
-		{
-		  if (QuietFlag == false)
-		    {
-		      if (SqrNorm(sp) >= Threshold)
-			cout << "Overlap |<"<<i<<"|"<<j<<">|^2 = " << SqrNorm(sp) << endl;
-		    }
-		  else
-		    {
-		      cout << SqrNorm(sp) << endl;
-		    }
-		}
-	      else
-		{
-		  if (QuietFlag == false)
-		    {
-		      if (fabs(sp)>=Threshold)
-			cout << "<"<<i<<"|"<<j<<"> = " << sp << endl;
-		    }
-		  else
-		    {
-		      cout << sp << endl;
-		    }
-		}
-
+	  for (int i = 0; i < VectorsAsAMatrix.GetNbrColumn(); ++i)
+	    {
+	      StateVectors[i + NbrVectors] = VectorsAsAMatrix[i];
 	    }
-	}
-      if (Manager.GetBoolean("sum") == true)
-	{
-	  if (QuietFlag == false)
-	    cout << "Total overlap = " << TotalOverlap << endl;
+	  Complex sp = 0.0;
+	  Complex TotalOverlap = 0.0;
+	  ComplexMatrix OverlapMatrix;
+	  if (MaxVectors == 1) 
+	    OverlapMatrix = ComplexMatrix(MaxVectors, NbrVectors - 1, true);
 	  else
-	    cout << TotalOverlap << endl;
+	    OverlapMatrix = ComplexMatrix(MaxVectors, NbrVectors, true);
+	  for (int i=0; i < MaxVectors; ++i)
+	    {
+	      if (Manager.GetBoolean("normalize"))
+		StateVectors[i] /= StateVectors[i].Norm();
+	      for (int j= i + 1; j < NbrVectors; ++j)
+		{	      
+		  if (Manager.GetBoolean("normalize"))
+		    StateVectors[j] /= StateVectors[j].Norm();
+		  sp = 0.0;
+		  if (Manager.GetBoolean("discard-sign"))
+		    for (int k = 0; k < StateVectors[i].GetVectorDimension(); ++k)
+		      sp += Norm(StateVectors[i][k] * StateVectors[j][k]);
+		  else
+		    if (Manager.GetBoolean("conjugate"))
+		      sp = EuclidianScalarProduct(StateVectors[i], StateVectors[j]);
+		    else
+		      sp = StateVectors[i] * StateVectors[j];
+		  if (Scalar == false)
+		    {
+		      TotalOverlap += SqrNorm(sp);
+		    }
+		  else
+		    {
+		      TotalOverlap += sp;
+		    }
+		  if (MaxVectors == 1)
+		    {
+		      OverlapMatrix.SetMatrixElement(0, j - 1, sp);
+		    }
+		  else
+		    {
+		      OverlapMatrix.SetMatrixElement(j, i, sp);
+		      OverlapMatrix.SetMatrixElement(i, j, sp);
+		      OverlapMatrix.SetMatrixElement(i, i, 1.0);
+		    }
+		  if (Scalar == false)
+		    {
+		      if (QuietFlag == false)
+			{
+			  if (SqrNorm(sp)>=Threshold)
+			    cout << "Overlap |<"<<i<<"|"<<j<<">|^2 = " << SqrNorm(sp) << endl;
+			}
+		      else
+			cout << SqrNorm(sp) << endl;
+		    }
+		  else
+		    {
+		      if (QuietFlag == false)
+			{
+			  if (Norm(sp)>=Threshold)
+			    {
+			      if (Manager.GetBoolean("polar"))
+				cout << "<"<<i<<"|"<<j<<"> = " << Norm(sp)<<"*Exp("<< Arg(sp)/M_PI<< " I Pi)" << endl;
+			      else
+				cout << "<"<<i<<"|"<<j<<"> = " << sp << endl;
+			    }
+			}
+		      else
+			cout << sp.Re << " " << sp.Im << endl;
+		    }
+		}
+	    }
+	  if (Manager.GetBoolean("sum") == true)
+	    {
+	      if (Scalar == false)
+		{
+		  if (QuietFlag == false)
+		    cout << "Total overlap = " << TotalOverlap.Re << endl;
+		  else
+		    cout << TotalOverlap.Re << endl;
+		}
+	      else
+		{
+		  if (QuietFlag == false)
+		    cout << "Total overlap = " << TotalOverlap << endl;
+		  else
+		    cout << TotalOverlap << endl;
+		}
+	    }
+	  if (Manager.GetString("matrix") != 0)
+	    {
+	      OverlapMatrix.WriteMatrix(Manager.GetString("matrix"));
+	    }
+	  delete[] StateVectors;
 	}
-      if (Manager.GetString("matrix") != 0)
+      else
 	{
-	  OverlapMatrix.WriteMatrix(Manager.GetString("matrix"));
+	  RealMatrix VectorsAsAMatrix;
+	  if (VectorsAsAMatrix.ReadMatrix(Manager.GetString("states-matrix")) == false)
+	    {
+	      cout << "error while reading matrix " << Manager.GetString("states-matrix") << endl;
+	      return -1;
+	    }
+	  RealVector* StateVectors = new RealVector [NbrVectors + VectorsAsAMatrix.GetNbrColumn()];
+	  for (int i = 0; i < NbrVectors; ++i)
+	    {
+	      if (StateVectors[i].ReadVector (VectorFiles[i]) == false)
+		{
+		  cout << "error while reading vector " << Manager.GetString(VectorFiles[i]) << endl;
+		  return -1;		  
+		}
+	      if (StateVectors[i].GetVectorDimension() != VectorsAsAMatrix.GetNbrRow())
+		{
+		  cout << "dimension mismacth bewteen vector " << Manager.GetString(VectorFiles[i]) << " (" << StateVectors[i].GetVectorDimension() << ") and "
+		       << Manager.GetString("states-matrix") << " (" << VectorsAsAMatrix.GetNbrRow() << ")" << endl;		  
+		}
+	    }
+	  for (int i = 0; i < VectorsAsAMatrix.GetNbrColumn(); ++i)
+	    {
+	      StateVectors[i + NbrVectors] = VectorsAsAMatrix[i];
+	    }
+	  double TotalOverlap = 0.0;
+	  double sp = 0.0;
+	  RealMatrix OverlapMatrix;
+	  if (MaxVectors == 1) 
+	    OverlapMatrix = RealMatrix(MaxVectors, NbrVectors - 1, true);
+	  else
+	    OverlapMatrix = RealMatrix(MaxVectors, NbrVectors, true);
+	  for (int i = 0; i < MaxVectors; ++i)
+	    {
+	      if (Manager.GetBoolean("normalize"))
+		StateVectors[i] /= StateVectors[i].Norm();
+	      for (int j = i + 1; j < NbrVectors; ++j)
+		{	      
+		  if (Manager.GetBoolean("normalize"))
+		    StateVectors[j] /= StateVectors[j].Norm();
+		  sp = 0.0;
+		  if (Manager.GetBoolean("discard-sign"))
+		    {
+		      for (int k = 0; k < StateVectors[i].GetVectorDimension(); ++k)
+			sp += fabs(StateVectors[i][k] * StateVectors[j][k]);
+		    }
+		  else
+		    {
+		      sp = StateVectors[i] * StateVectors[j] ;
+		    }
+		  if (MaxVectors == 1)
+		    {
+		      OverlapMatrix.SetMatrixElement(0, j - 1, sp);
+		    }
+		  else
+		    {
+		      OverlapMatrix.SetMatrixElement(j, i, sp);
+		      OverlapMatrix.SetMatrixElement(i, j, sp);
+		      OverlapMatrix.SetMatrixElement(i, i, 1.0);
+		    }
+		  if (Scalar == false)
+		    {
+		      TotalOverlap += SqrNorm(sp);
+		    }
+		  else
+		    {
+		      TotalOverlap += sp;
+		    }
+		  if (Scalar == false)
+		    {
+		      if (QuietFlag == false)
+			{
+			  if (SqrNorm(sp) >= Threshold)
+			    cout << "Overlap |<"<<i<<"|"<<j<<">|^2 = " << SqrNorm(sp) << endl;
+			}
+		      else
+			{
+			  cout << SqrNorm(sp) << endl;
+			}
+		    }
+		  else
+		    {
+		      if (QuietFlag == false)
+			{
+			  if (fabs(sp)>=Threshold)
+			    cout << "<"<<i<<"|"<<j<<"> = " << sp << endl;
+			}
+		      else
+			{
+			  cout << sp << endl;
+			}
+		    }
+		  
+		}
+	    }
+	  if (Manager.GetBoolean("sum") == true)
+	    {
+	      if (QuietFlag == false)
+		cout << "Total overlap = " << TotalOverlap << endl;
+	      else
+		cout << TotalOverlap << endl;
+	    }
+	  if (Manager.GetString("matrix") != 0)
+	    {
+	      OverlapMatrix.WriteMatrix(Manager.GetString("matrix"));
+	    }
+	  delete[] StateVectors;
 	}
     }
   return 0;
